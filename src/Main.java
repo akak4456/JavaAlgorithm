b1e9a31ca71622f700765afa4ee05617fc67878b
akak4456 <akak4456@naver.com>
2024-06-25 22:16:50 +0900

    [CNG] 24/06/25 알고리즘 풀이 진행중 (12850번: 본대 산책 2 시도중)

--- src/Main.java
+++ src/Main.java
@@ -288,124 +288,124 @@ class Pair implements Comparable<Pair> {
 }
 
 public class Main {
-	private static int N;
-	private static int[][] board;
-	private static int blackMaxCnt = 0;
-	private static int whiteMaxCnt = 0;
-	private static void printBoard() {
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				System.out.print(board[i][j] + " ");
+	private static int D;
+	private static long[] arr;
+	private static long[][] cnt;
+	private static final long MOD= 1_000_000_007;
+	private static ArrayList<ArrayList<Integer>> graph;
+	private static void printArr() {
+		for(int i=0;i<8;i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	private static void printCnt() {
+		for(int i=0;i<8;i++) {
+			System.out.print(i + ":");
+			for(int j=0;j<8;j++) {
+				System.out.print(cnt[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
-	private static void solve(int row, int column, boolean isBlack, int curCount) {
-		if(isBlack) {
-			if(curCount > blackMaxCnt) {
-				blackMaxCnt = curCount;
-			}
-		} else {
-			if(curCount > whiteMaxCnt) {
-				whiteMaxCnt = curCount;
-			}
-		}
-		if(row >= N || column >= N) return;
-		int newRow = row;
-		int newColumn = column + 2;
-		if(newColumn >= N) {
-			if(N % 2 == 0) {
-				if(row % 2 == 0) {
-					if(isBlack) {
-						newColumn = 1;
-					} else {
-						newColumn = 0;
-					}
-				} else {
-					if(isBlack) {
-						newColumn = 0;
-					} else {
-						newColumn = 1;
-					}
-				}
-				newRow++;
-			} else {
-				newRow++;
-				newColumn = newColumn % N;
-			}
-		}
-		
-		solve(newRow, newColumn, isBlack, curCount);
-		
-		if(board[row][column] == 1) {
-			int nrow = row;
-			int ncol = column;
-			boolean isCan = true;
-			while(nrow >= 0 && ncol >= 0) {
-				if(board[nrow][ncol] == 2) {
-					isCan = false;
-					break;
-				}
-				nrow--;
-				ncol--;
-			}
-			if(isCan) {
-				nrow = row;
-				ncol = column;
-				while(nrow < N && ncol < N) {
-					if(board[nrow][ncol] == 2) {
-						isCan = false;
-						break;
-					}
-					nrow++;
-					ncol++;
-				}
-			}
-			if(isCan) {
-				nrow = row;
-				ncol = column;
-				while(nrow < N && ncol >= 0) {
-					if(board[nrow][ncol] == 2) {
-						isCan = false;
-						break;
-					}
-					nrow++;
-					ncol--;
-				}
-			}
-			if(isCan) {
-				nrow = row;
-				ncol = column;
-				while(nrow >= 0 && ncol < N) {
-					if(board[nrow][ncol] == 2) {
-						isCan = false;
-						break;
-					}
-					nrow--;
-					ncol++;
-				}
-			}
-			if(isCan) {
-				board[row][column] = 2;
-				solve(newRow, newColumn, isBlack, curCount + 1);
-				board[row][column] = 1;
-			}
-		}
-	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		for(int i=0;i<N;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for(int j=0;j<N;j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(br.readLine());
+		/*
+		 4
+		# 답 : 11
+
+		32
+		# 답 : 622921825
+		
+		100000000
+		# 답 : 261245548
+		node 0:정보과학관
+		node 1:전산관
+		node 2:미래관
+		node 3:신양관
+		node 4:진리관
+		node 5:한경직기념관
+		node 6:학생회관
+		node 7:형남공학관 
+		8
+		1 0 0 0 0 0 0 0 
+		0 1 1 0 0 0 0 0 
+		2 1 1 2 0 1 0 0 
+		2 5 6 3 3 3 0 1 
+		11 11 13 17 6 13 4 3 
+		24 41 52 43 34 39 9 17 
+		93 119 147 166 91 146 51 48 
+		266 406 524 503 363 452 139 197 
+		930 1293 1627 1745 1094 1587 560 591 
+		
+		930 = 406 + 524
+		    = (93 + 147 + 166) + (93 + 119 + 166 + 146)
+		 */
+		graph = new ArrayList<>();
+		for(int i=0;i<8;i++) {
+			graph.add(new ArrayList<>());
+		}
+		graph.get(0).add(1);
+		graph.get(0).add(2);
+		
+		graph.get(1).add(0);
+		graph.get(1).add(2);
+		graph.get(1).add(3);
+		
+		graph.get(2).add(0);
+		graph.get(2).add(1);
+		graph.get(2).add(3);
+		graph.get(2).add(5);
+		
+		graph.get(3).add(1);
+		graph.get(3).add(2);
+		graph.get(3).add(4);
+		graph.get(3).add(5);
+		
+		graph.get(4).add(3);
+		graph.get(4).add(5);
+		graph.get(4).add(6);
+		
+		graph.get(5).add(2);
+		graph.get(5).add(3);
+		graph.get(5).add(4);
+		graph.get(5).add(7);
+		
+		graph.get(6).add(4);
+		graph.get(6).add(7);
+		
+		graph.get(7).add(5);
+		graph.get(7).add(6);
+		arr = new long[8];
+		cnt = new long[8][8];
+		arr[0] = 1;// 0분일 때
+		for(int i=1;i<=D;i++) {
+			// System.out.println("D:"+i);
+			printArr();
+			long[] tmp = new long[8];
+			long[][] newCnt = new long[8][8];
+			for(int j=0;j<8;j++) {
+				for(int k=0;k<graph.get(j).size();k++) {
+					tmp[graph.get(j).get(k)] = (tmp[graph.get(j).get(k)] + arr[j]) % MOD;
+					if(i == 1) {
+						cnt[j][graph.get(j).get(k)]++;
+					} else {
+						for(int t=0;t<8;t++) {
+							newCnt[j][graph.get(j).get(k)] += cnt[graph.get(j).get(k)][t];
+						}
+					}
+				}
+			}
+			arr = tmp;
+			if(i > 1) {
+				cnt = newCnt;
+			}
+			if(i == D/2 ) {
+				printCnt();
 			}
 		}
-		solve(0,0,true, 0);
-		solve(0,1,false, 0);
-		// System.out.println(blackMaxCnt);
-		// System.out.println(whiteMaxCnt);
-		System.out.println(blackMaxCnt + whiteMaxCnt);
+		printArr();
+		System.out.println(arr[0]);
 	}  
 }
