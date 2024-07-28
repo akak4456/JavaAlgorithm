1414b51e6016f2b67052e7fd0676afa09134a929
akak4456 <akak4456@naver.com>
2024-07-28 14:11:28 +0900

    [CNG] 24/07/28 알고리즘 문제 풀이 진행함

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, M;
-	private static int[] arr;
-	private static int[] sum;
-	private static int[] group;
-	public static void main(String[] args) throws Exception {
+	private static int N;
+	private static int[][] board;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N + 1];
-		sum = new int[N + 1];
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i=1;i<=N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			sum[i] = sum[i - 1]  + arr[i];
-		}
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<M;i++) {
-			int a,b;
-			st = new StringTokenizer(br.readLine(), " ");
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			sb.append(sum[b] - sum[a - 1]);
-			sb.append("\n");
+		N = Integer.parseInt(br.readLine().trim());
+		board = new int[N][2];
+		for (int n = 0; n < N; n++) {
+			StringTokenizer st = new StringTokenizer(br.readLine().trim());
+			board[n][0] = Integer.parseInt(st.nextToken());
+			board[n][1] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(sb);
+		Arrays.sort(board, Comparator.comparingInt(o1 -> o1[0]));
+		int[] arr = Arrays.stream(board).mapToInt(o -> o[1]).toArray();
+		int[] D = new int[N];
+		int[] result = new int[N];
+		D[0] = arr[0];
+		int size = 1;
+		for (int i = 1; i < N; i++) {
+			int index = Arrays.binarySearch(D, 0, size, arr[i]);
+			index = index >= 0 ? index : Math.abs(index) - 1;
+			D[index] = arr[i];
+			result[i] = index;
+			if (index == size) size++;
+		}
+
+		int s = size-1, i = N-1;
+		boolean[] vtd = new boolean[N];
+		while (s >= 0) {
+			if (result[i] == s) {
+				vtd[i] = true;
+				s--;
+			}
+			i--;
+		}
+		int count = 0;
+		for (int n = 0; n < N; n++) {
+			if (!vtd[n]) {
+				count++;
+				sb.append(board[n][0]).append("\n");
+			}
+		}
+		System.out.println(count);
+		if (count > 0) {
+			sb.setLength(sb.length()-1);
+			System.out.println(sb);
+		}
 	}
 }
