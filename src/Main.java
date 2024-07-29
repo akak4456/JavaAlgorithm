a4224ee80f0e0e91363a2f7f2289d7ad60e6a11e
DESKTOP-2UK7O3T\jo <akak4456@naver.com>
2024-07-30 08:50:28 +0900

    [CNG] 24/07/30 알고리즘 문제 풀이 진행함

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int M;
+	private static int N, M;
 	private static int mask;
 	private static int allMask;
+	private static boolean[][] adj;
+	private static boolean[] visited;
+	private static int ans;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		M = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for(int i=1;i<=20;i++) {
-			allMask |= (1<<i);
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		adj = new boolean[N + 1][N + 1];
+		visited = new boolean[N + 1];
+		for(int i=0;i < M;i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adj[a][b] = true;
+			adj[b][a] = true;
 		}
-		for(int i=0;i<M;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			String op = st.nextToken();
-			switch(op) {
-				case "add":
-					int operand = Integer.parseInt(st.nextToken());
-					mask |= (1 << operand);
-					break;
-				case "remove":
-					operand = Integer.parseInt(st.nextToken());
-					mask &= ~(1 << operand);
-					break;
-				case "check":
-					operand = Integer.parseInt(st.nextToken());
-					boolean isChecked = (mask & (1 << operand)) > 0;
-					if(isChecked) {
-						sb.append("1\n");
-					} else {
-						sb.append("0\n");
-					}
-					break;
-				case "toggle":
-					operand = Integer.parseInt(st.nextToken());
-					mask ^= (1 << operand);
-					break;
-				case "all":
-					mask = allMask;
-					break;
-				case "empty":
-					mask = 0;
-					break;
+		for(int i=1;i<=N;i++) {
+			if(!visited[i]) {
+				ans++;
+				dfs(i);
 			}
 		}
-		System.out.println(sb);
+		System.out.println(ans);
+	}
+
+	private static void dfs(int node) {
+		if(visited[node]) return;
+		visited[node] = true;
+		for(int other=1;other<=N;other++) {
+			if(node == other || visited[other]) continue;
+			if(adj[node][other]) {
+				dfs(other);
+			}
+		}
 	}
 }
