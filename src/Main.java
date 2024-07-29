d80db9213e71a4bec51b9daa133d04443ead6740
DESKTOP-2UK7O3T\jo <akak4456@naver.com>
2024-07-29 09:05:48 +0900

    [CNG] 24/07/29 알고리즘 문제 풀이 진행함

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,51 @@
 import java.io.*;
 import java.util.*;
-class Node implements Comparable<Node> {
-	int start;
-	int end;
-
-
-	@Override
-	public int compareTo(Node o) {
-		return Integer.compare(start, o.start);
-	}
-
-	@Override
-	public String toString() {
-		return "Node{" +
-				"start=" + start +
-				", end=" + end +
-				'}';
-	}
-}
 
 public class Main {
-	private static int N;
-	private static Node[] nodes;
-	private static int L;
-	private static ArrayList<Integer> start;
-	private static ArrayList<Integer> end;
+	private static int M;
+	private static int mask;
+	private static int allMask;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		nodes = new Node[N];
-		for (int i = 0; i < N; i++) {
+		M = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=1;i<=20;i++) {
+			allMask |= (1<<i);
+		}
+		for(int i=0;i<M;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			nodes[i] = new Node();
-			nodes[i].start = Math.min(a, b);
-			nodes[i].end = Math.max(a, b);
-		}
-		L = Integer.parseInt(br.readLine());
-		start = new ArrayList<>();
-		end = new ArrayList<>();
-		for(int i=0;i<N;i++) {
-			if(nodes[i].end - nodes[i].start <= L) {
-				start.add(nodes[i].start);
-				end.add(nodes[i].end);
+			String op = st.nextToken();
+			switch(op) {
+				case "add":
+					int operand = Integer.parseInt(st.nextToken());
+					mask |= (1 << operand);
+					break;
+				case "remove":
+					operand = Integer.parseInt(st.nextToken());
+					mask &= ~(1 << operand);
+					break;
+				case "check":
+					operand = Integer.parseInt(st.nextToken());
+					boolean isChecked = (mask & (1 << operand)) > 0;
+					if(isChecked) {
+						sb.append("1\n");
+					} else {
+						sb.append("0\n");
+					}
+					break;
+				case "toggle":
+					operand = Integer.parseInt(st.nextToken());
+					mask ^= (1 << operand);
+					break;
+				case "all":
+					mask = allMask;
+					break;
+				case "empty":
+					mask = 0;
+					break;
 			}
 		}
-		Collections.sort(start);
-		Collections.sort(end);
-		int ans = 0;
-		if(!start.isEmpty()) {
-			int startCnt = 0;
-			int startTarget = start.get(0);
-			for (int i = 0; i < start.size(); i++) {
-				if (startTarget != start.get(i)) {
-					startCnt = i;
-					startTarget = start.get(i);
-				}
-				int endIdx = upper(end, start.get(i) + L);
-				int endCnt = end.size() - endIdx;
-				ans = Math.max(ans, end.size() - (startCnt + endCnt));
-			}
-		}
-		System.out.println(ans);
-	}
-
-	private static int upper(ArrayList<Integer> list, int key) {
-		int start = 0;
-		int end = list.size() - 1;
-		while(start <= end) {
-			int mid = (start + end) / 2;
-			if(list.get(mid) <= key) {
-				start = mid + 1;
-			} else {
-				end = mid - 1;
-			}
-		}
-		return start;
+		System.out.println(sb);
 	}
 }
