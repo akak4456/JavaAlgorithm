37d986af0cd42de449dabc487eb2fc53fad24d1b
DESKTOP-2UK7O3T\jo <akak4456@naver.com>
2024-07-31 09:05:29 +0900

    [CNG] 24/07/31 알고리즘 문제 풀이 진행함

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, M;
-	private static int mask;
-	private static int allMask;
-	private static boolean[][] adj;
-	private static boolean[] visited;
-	private static int ans;
+	private static int N;
+
+	private static int[] dp;
+
+	private static int solve(int n) {
+		if(n <= 1) {
+			return 1;
+		}
+		if(dp[n] != -1) {
+			return dp[n];
+		}
+		dp[n] = solve(n - 1) % 10007;
+		dp[n] = (dp[n] + solve(n - 2)) % 10007;
+		return dp[n];
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		adj = new boolean[N + 1][N + 1];
-		visited = new boolean[N + 1];
-		for(int i=0;i < M;i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			adj[a][b] = true;
-			adj[b][a] = true;
-		}
-		for(int i=1;i<=N;i++) {
-			if(!visited[i]) {
-				ans++;
-				dfs(i);
-			}
-		}
-		System.out.println(ans);
-	}
-
-	private static void dfs(int node) {
-		if(visited[node]) return;
-		visited[node] = true;
-		for(int other=1;other<=N;other++) {
-			if(node == other || visited[other]) continue;
-			if(adj[node][other]) {
-				dfs(other);
-			}
-		}
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N + 1];
+		Arrays.fill(dp, -1);
+		System.out.println(solve(N));
 	}
 }
