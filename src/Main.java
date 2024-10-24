2bc6fb4b16e541fe11d000005ec9b8685e31e947
akak4456 <akak4456@naver.com>
2024-10-24 10:25:18 +0900

    2024.10.24

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,62 @@
 import java.io.*;
 import java.util.*;
 class Node {
-	int data;
-	int idx;
+	int row;
+	int col;
+
+	Node(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
 }
 public class Main {
-	private static int N;
+	private static int N, M;
 	private static Node[] nodes;
 	private static int[] ans;
+	private static char[][] board;
+	private static Queue<Node> q;
+	private static final int[] dcol = {0,0,-1,1};
+	private static final int[] drow = {-1,1,0,0};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		nodes = new Node[N];
-		ans = new int[N];
-		for (int i = 0; i < N; i++) {
-			nodes[i] = new Node();
-			nodes[i].data = Integer.parseInt(st.nextToken());
-			nodes[i].idx = i;
-		}
-		Arrays.sort(nodes, Comparator.comparingInt(o -> o.data));
-		int newCoordinate = 0;
-		int curData = nodes[0].data;
-		for (int i = 0; i < N; i++) {
-			if (nodes[i].data != curData) {
-				newCoordinate++;
-				curData = nodes[i].data;
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		board = new char[N][M];
+		q = new LinkedList<>();
+		for(int i=0;i<N;i++) {
+			String tmp = br.readLine();
+			for(int j=0;j<M;j++) {
+				board[i][j] = tmp.charAt(j);
+				if(board[i][j] == 'I') {
+					q.add(new Node(i, j));
+				}
 			}
-			ans[nodes[i].idx] = newCoordinate;
 		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			sb.append(ans[i]).append(" ");
+
+		int cnt = 0;
+		while(!q.isEmpty()) {
+			Node cur = q.poll();
+			if(board[cur.row][cur.col] == 'V') {
+				continue;
+			}
+			if(board[cur.row][cur.col] == 'P') {
+				cnt++;
+			}
+			board[cur.row][cur.col] = 'V';
+			for(int i=0;i<4;i++) {
+				int nrow = cur.row + drow[i];
+				int ncol = cur.col + dcol[i];
+				if(nrow < 0 || nrow >= N || ncol < 0 || ncol >= M || board[nrow][ncol] == 'X' || board[nrow][ncol] == 'V' ) {
+					continue;
+				}
+				q.add(new Node(nrow, ncol));
+			}
 		}
-		System.out.println(sb);
+		if(cnt == 0) {
+			System.out.println("TT");
+		} else {
+			System.out.println(cnt);
+		}
 	}
 }
