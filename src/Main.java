44f5156f138f99bf03176f86d2bc7d00c330bec1
akak4456 <akak4456@naver.com>
2024-10-28 12:33:50 +0900

    2024.10.28

--- src/Main.java
+++ src/Main.java
@@ -3,70 +3,66 @@ import java.util.*;
 class Node {
 	int row;
 	int col;
-	int curDist;
+	int curDay;
 
 	Node(int row, int col) {
 		this.row = row;
 		this.col = col;
-		this.curDist = 0;
+		this.curDay = 0;
 	}
 
-	Node(int row, int col, int curDist) {
+	Node(int row, int col, int curDay) {
 		this.row = row;
 		this.col = col;
-		this.curDist = curDist;
+		this.curDay = curDay;
 	}
 }
 public class Main {
-	private static int n, m;
-	private static final int[] dcol = {0,0,-1,1};
-	private static final int[] drow = {-1,1,0,0};
+	private static int M, N;
 	private static int[][] board;
-	private static Queue<Node> q;
-	private static int[][] ans;
+	private static Queue<Node> q = new LinkedList<>();
+	private static final int[] drow = {-1,1,0,0};
+	private static final int[] dcol = {0,0,-1,1};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		board = new int[n][m];
-		ans = new int[n][m];
-		q = new LinkedList<>();
-		for (int i = 0; i < n; i++) {
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		board = new int[N][M];
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < m; j++) {
+			for (int j = 0; j < M; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
-				if(board[i][j] == 2) {
-					q.add(new Node(i, j));
+				if(board[i][j] == 1) {
+					q.add(new Node(i,j));
 				}
 			}
 		}
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				if(board[i][j] > 0) {
-					ans[i][j] = -1;
-				}
-			}
-		}
+		int ans = 0;
 		while(!q.isEmpty()) {
 			Node cur = q.poll();
-			if(board[cur.row][cur.col] == 0) continue;
-			board[cur.row][cur.col] = 0;
-			ans[cur.row][cur.col] = cur.curDist;
+			if(board[cur.row][cur.col] == 2) continue;
+			board[cur.row][cur.col] = 2;
+			ans = Math.max(ans, cur.curDay);
 			for(int i=0;i<4;i++) {
 				int nrow = cur.row + drow[i];
 				int ncol = cur.col + dcol[i];
-				if(nrow < 0 || nrow >= n || ncol < 0 || ncol >= m || board[nrow][ncol] == 0) continue;
-				q.add(new Node(nrow, ncol, cur.curDist + 1));
+				if(nrow < 0 || nrow >= N || ncol < 0 || ncol >= M || board[nrow][ncol] == -1 || board[nrow][ncol] == 1 || board[nrow][ncol] == 2) continue;
+				q.add(new Node(nrow, ncol, cur.curDay + 1));
 			}
 		}
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				sb.append(ans[i][j]).append(" ");
+		boolean isPossible = true;
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				if(board[i][j] == 0) {
+					isPossible = false;
+				}
 			}
-			sb.append("\n");
 		}
-		System.out.println(sb);
+		if(isPossible) {
+			System.out.println(ans);
+		} else {
+			System.out.println(-1);
+		}
 	}
 }
