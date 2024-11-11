210b47bc44c444e2dd9b33dde9bf7ea305779e0a
akak4456 <akak4456@naver.com>
2024-11-11 19:59:53 +0900

    2024.11.11

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,47 @@
 import java.io.*;
 import java.util.*;
-class Node {
-	int startIdx;
-	int moreValue;
-
-	Node(int startIdx, int moreValue) {
-		this.startIdx = startIdx;
-		this.moreValue = moreValue;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Node node = (Node) o;
-		return startIdx == node.startIdx && moreValue == node.moreValue;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(startIdx, moreValue);
-	}
-}
 public class Main {
 	private static int N;
-	private static int A[];
-	private static int D[];
-	private static int X[];
-	private static int curXSize;
+	private static ArrayList<ArrayList<Integer>> graph;
+	private static int[] parent;
+	private static boolean[] visited;
+	private static Queue<Integer> q;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		A = new int[N + 1];
-		D = new int[N + 1];
-		X = new int[N + 1];
-		curXSize = 1;
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for(int i=1;i<=N;i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+		graph = new ArrayList<>();
+		parent = new int[N + 1];
+		visited = new boolean[N + 1];
+		Arrays.fill(parent, -1);
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
 		}
-
-		for(int i=1;i<=N;i++) {
-			if(A[i] > X[curXSize - 1]) {
-				X[curXSize] = A[i];
-				D[i] = curXSize;
-				curXSize++;
-			} else {
-				int startIdx = 0;
-				int endIdx = curXSize - 1;
-				while(startIdx < endIdx) {
-					int mid = (startIdx + endIdx) / 2;
-					if(X[mid] >= A[i]) {
-						endIdx = mid;
-					} else {
-						startIdx = mid + 1;
-					}
+		q = new LinkedList<>();
+		for (int i = 0; i < N - 1; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph.get(a).add(b);
+			graph.get(b).add(a);
+		}
+		q.add(1);
+		while(!q.isEmpty()) {
+			int node = q.poll();
+			// System.out.println(node);
+			if(visited[node]) continue;
+			visited[node] = true;
+			for(int i=0;i<graph.get(node).size();i++) {
+				int adj = graph.get(node).get(i);
+				if(parent[adj] == -1) {
+					parent[adj] = node;
 				}
-				X[endIdx] = A[i];
-				D[i] = endIdx;
+				q.add(adj);
 			}
 		}
-
-		int ans = 0;
-		for(int i=0;i<=N;i++) {
-			if(D[i] > ans) {
-				ans = D[i];
-			}
+		StringBuilder sb = new StringBuilder();
+		for(int i=2;i<=N;i++) {
+			sb.append(parent[i]).append("\n");
 		}
-		System.out.println(ans);
+		System.out.println(sb);
 	}
 }
