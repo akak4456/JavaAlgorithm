76e100cb7b06d1750aa85de48e1d2c1709188c3c
akak4456 <akak4456@naver.com>
2024-11-17 20:40:25 +0900

    2024.11.17

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,27 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	private static int N;
-	private static int[][] dp;
-	private static int[][] RGB;
-	private static int solve(int idx, int prevColorIdx) {
-		int ret = 987654321;
-		if(idx == N) return 0;
-		if(dp[idx][prevColorIdx] != -1) {
-			return dp[idx][prevColorIdx];
+	private static int A, B, C;
+	private static int solve(int num, int pow, int mod) {
+		int ret = 1;
+		if(pow == 0){
+			return 1;
 		}
-		for(int i=1;i<=3;i++) {
-			if(prevColorIdx == i) continue;
-			ret = Math.min(ret, RGB[idx][i] + solve(idx + 1, i));
+		if(pow % 2 == 0) {
+			int t = solve(num,pow/2,mod);
+			ret = (ret * t) % mod;
+			ret = (ret * t) % mod;
+		} else {
+			ret = (num * solve(num, pow - 1, mod)) % mod;
 		}
-		dp[idx][prevColorIdx] = ret;
-		return ret;
+		return ret % mod;
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		dp = new int[N][4];
-		RGB = new int[N][4];
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<4;j++) {
-				dp[i][j] = -1;
-			}
-		}
-		for(int i=0;i<N;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=1;j<=3;j++) {
-				RGB[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		System.out.println(solve(0, 0));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		System.out.println(solve(A,B,C));
 	}
 }
