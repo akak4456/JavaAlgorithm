81e0655db94fa63d1004d008e0def8d80a49e1fd
akak4456 <akak4456@naver.com>
2024-11-20 21:42:48 +0900

    2024.11.20

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,66 @@
 import java.io.*;
 import java.util.*;
+class Node {
+	char ch;
+	Node left, right;
+}
 public class Main {
-	private static int n;
-	private static int[][] board;
-	private static int[][] dp;
-	private static int solve(int row, int col) {
-		if (row == n - 1) {
-			return board[row][col];
+	private static int N;
+	private static Node[] nodes;
+	private static String preorder(Node root) {
+		String ret = root.ch + "";
+		if (root.left != null) {
+			ret += preorder(root.left);
 		}
-		if(dp[row][col] != -1) return dp[row][col];
-		int ret = board[row][col] + solve(row + 1, col);
-		ret = Math.max(ret,board[row][col] + solve(row+1,col+1));
-		dp[row][col] = ret;
+		if(root.right != null) {
+			ret += preorder(root.right);
+		}
+		return ret;
+	}
+	private static String inorder(Node root) {
+		String ret = "";
+		if(root.left != null) {
+			ret += inorder(root.left);
+		}
+		ret += root.ch;
+		if(root.right != null) {
+			ret += inorder(root.right);
+		}
+		return ret;
+	}
+	private static String postorder(Node root) {
+		String ret = "";
+		if(root.left != null) {
+			ret += postorder(root.left);
+		}
+		if(root.right != null) {
+			ret += postorder(root.right);
+		}
+		ret += root.ch;
 		return ret;
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		board = new int[n][n];
-		dp = new int[n][n];
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				dp[i][j] = -1;
-			}
+		N = Integer.parseInt(br.readLine());
+		nodes = new Node[N];
+		for (int i = 0; i < N; i++) {
+			nodes[i] = new Node();
+			nodes[i].ch = (char) (i + 'A');
 		}
-		for(int i=0;i<n;i++) {
+		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0;j<=i;j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+			char a = st.nextToken().charAt(0);
+			char b = st.nextToken().charAt(0);
+			char c = st.nextToken().charAt(0);
+			if(b != '.') {
+				nodes[a - 'A'].left = nodes[b - 'A'];
+			}
+			if(c != '.') {
+				nodes[a-'A'].right = nodes[c - 'A'];
 			}
 		}
-		System.out.println(solve(0,0));
+		System.out.println(preorder(nodes[0]));
+		System.out.println(inorder(nodes[0]));
+		System.out.println(postorder(nodes[0]));
 	}
 }
