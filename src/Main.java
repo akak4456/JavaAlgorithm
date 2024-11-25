61ae72d442281383a97990f9f09e093a71794e57
akak4456 <akak4456@naver.com>
2024-11-25 10:19:45 +0900

    2024.11.25

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,96 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Stack;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
-class Point {
-	long x;
-	long y;
-	Point(long x, long y) {
-		this.x = x;
-		this.y = y;
+
+class TreeNode {
+	int adjNode;
+	int dist;
+	TreeNode(int adjNode, int dist) {
+		this.adjNode = adjNode;
+		this.dist = dist;
 	}
 }
 public class Main {
 	private static int N;
-	private static Point[] points;
-	private static Point root = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
-	private static int ccw(Point p1, Point p2, Point p3) {
-		long result = (p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p2.x * p1.y + p3.x * p2.y + p1.x * p3.y);
-		if(result > 0) {
-			return 1;
-		} else if(result < 0) {
-			return -1;
-		} else {
-			return 0;
-		}
-	}
-	private static long dist(Point p1, Point p2) {
-		return (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
-	}
-	private static int convexHull() {
-		for(int i=0;i<N;i++) {
-			if(points[i].y < root.y) {
-				root = points[i];
-			} else if(points[i].y == root.y && points[i].x < root.x) {
-				root = points[i];
-			}
-		}
-		Arrays.sort(points, new Comparator<Point>() {
-			@Override
-			public int compare(Point p1, Point p2) {
-				int result = ccw(root, p1, p2);
-				if(result > 0) {
-					return -1;
-				} else if(result < 0) {
-					return 1;
-				} else {
-					long dist1 = dist(root, p1);
-					long dist2 = dist(root, p2);
-					if(dist1 > dist2) {
-						return 1;
-					}
-				}
-				return -1;
-			}
-		});
+	private static ArrayList<ArrayList<TreeNode>> adj;
+	private static int[][] parent;
+	private static int[] level;
+	private static final int maxLevel = 20;
+	private static int[] distFromRoot;
+	private static int M;
+	private static void setTree(int node, int pnode, int lv, int dFromRoot) {
+		level[node] = lv;
+		parent[node][0] = pnode;
+		distFromRoot[node] = dFromRoot;
 
-		Stack<Point> stack = new Stack<>();
-		stack.add(root);
-		for(int i=1;i<N;i++) {
-			while(stack.size() > 1 && (ccw(stack.get(stack.size() -2), stack.get(stack.size() - 1), points[i]) <= 0)) {
-				stack.pop();
-			}
-			stack.add(points[i]);
+		for(int i=1;i<=maxLevel;i++) {
+			parent[node][i] = parent[parent[node][i-1]][i-1];
 		}
-		return stack.size();
+		for(int i=0;i<adj.get(node).size();i++) {
+			TreeNode childNode = adj.get(node).get(i);
+			if(childNode.adjNode == pnode) continue;
+			setTree(childNode.adjNode, node, lv+1, dFromRoot + childNode.dist);
+		}
+	}
+	private static int LCA(int a, int b) {
+		if(a == 1 || b == 1) return 1;
+		int target = a;
+		int compare = b;
+		if(level[a] < level[b]) {
+			int tmp = target;
+			target = compare;
+			compare = tmp;
+		}
+		if(level[target] != level[compare]) {
+			for(int i=maxLevel; i>=0;i--) {
+				if(level[parent[target][i]] >= level[compare]) {
+					target = parent[target][i];
+				}
+			}
+		}
+		int ret = target;
+		if(target != compare) {
+			for(int i=maxLevel; i>=0;i--) {
+				if(parent[target][i] != parent[compare][i]) {
+					target = parent[target][i];
+					compare = parent[compare][i];
+				}
+				ret = parent[target][i];
+			}
+		}
+		return ret;
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		points = new Point[N];
-		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			long x = Integer.parseInt(st.nextToken());
-			long y = Integer.parseInt(st.nextToken());
-			points[i] = new Point(x, y);
+		adj = new ArrayList<>();
+		for(int i=0;i<=N;i++) {
+			adj.add(new ArrayList<>());
 		}
-		System.out.println(convexHull());
+		for(int i=0;i<N - 1;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			adj.get(a).add(new TreeNode(b,c));
+			adj.get(b).add(new TreeNode(a,c));
+		}
+		parent = new int[N + 1][maxLevel + 1];
+		level = new int[N + 1];
+		distFromRoot = new int[N + 1];
+		setTree(1, 0, 1, 0);
+		M = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int testCase=0;testCase<M;testCase++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int lca = LCA(a, b);
+			sb.append((distFromRoot[a] + distFromRoot[b] - (distFromRoot[lca] * 2)));
+			sb.append('\n');
+		}
+		System.out.println(sb);
 	}
 }
