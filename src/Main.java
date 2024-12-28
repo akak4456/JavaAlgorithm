75e1610a93255a405ba547e3b21e57b75463a0f1
akak4456 <akak4456@naver.com>
2024-12-28 10:50:28 +0900

    [CNG] 10171번: 고양이 풀었음

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,9 @@ import java.util.StringTokenizer;
 public class Main {
 	private static int G;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		G = Integer.parseInt(br.readLine());
-		if(90 <= G && G <= 100) {
-			System.out.println("A");
-		} else if(80 <= G && G < 90) {
-			System.out.println("B");
-		} else if(70 <= G && G < 80) {
-			System.out.println("C");
-		} else if(60 <= G && G < 70) {
-			System.out.println("D");
-		} else {
-			System.out.println("F");
-		}
+		System.out.println("\\    /\\\n" +
+				" )  ( ')\n" +
+				"(  /  )\n" +
+				" \\(__)|");
 	}
 }
