5b4adf5e329e09b2da1cc9995cf24822b460cfc2
akak4456 <akak4456@naver.com>
2024-12-29 12:30:34 +0900

    [CNG] 10871번: X보다 작은 수 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
-	private static int A, B;
+	private static int N, X;
+	private static int arr[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-		System.out.println(A + B);
-		System.out.println(A - B);
-		System.out.println(A * B);
-		System.out.println(A / B);
-		System.out.println(A % B);
+		N = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i=0;i<N;i++) {
+			if (arr[i] < X) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+		System.out.println();
 	}
 }
