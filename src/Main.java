76b36c81d236d1fbdb7450bc7d7b5107e32917e1
akak4456 <akak4456@naver.com>
2024-12-29 12:27:48 +0900

    [CNG] 10869번: 사칙연산 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
+	private static int A, B;
 	public static void main(String[] args) throws IOException {
-		System.out.println("|\\_/|\n" +
-				"|q p|   /}\n" +
-				"( 0 )\"\"\"\\\n" +
-				"|\"^\"`    |\n" +
-				"||_/=\\\\__|");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
 	}
 }
