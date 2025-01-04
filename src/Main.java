6bccd577ebc8c82d99f223cdd8b4879c16f5aaf8
akak4456 <akak4456@naver.com>
2025-01-04 14:09:55 +0900

    [CNG] 10952번: A+B - 5 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 public class Main {
-	private static int T;
-	public static void main(String[] args) throws Exception {
-		Scanner in = new Scanner(System.in);
-
-		while(in.hasNextInt()) {
-			int A = in.nextInt();
-			int B = in.nextInt();
-			System.out.println(A + B);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A, B;
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			if(A == 0 && B == 0) break;
+			System.out.println(A+B);
 		}
-
-		in.close();
 	}
 }
