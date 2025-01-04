b3c0625a22d7da3f9548eefc9c196bcd3da2588a
akak4456 <akak4456@naver.com>
2025-01-04 14:09:29 +0900

    [CNG] 10951번: A+B - 4 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 public class Main {
 	private static int T;
-	private static int A, B;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
-		for(int testCase = 0; testCase < T; testCase++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		Scanner in = new Scanner(System.in);
+
+		while(in.hasNextInt()) {
+			int A = in.nextInt();
+			int B = in.nextInt();
 			System.out.println(A + B);
 		}
+
+		in.close();
 	}
 }
