3a8115ea9228d2e41ce856f7022b0e36aa58d117
akak4456 <akak4456@naver.com>
2025-01-05 12:05:23 +0900

    [CNG] 1152번: 단어의 개수 풀었음

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,7 @@ public class Main {
 	private static int arr[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int minVal = Integer.MAX_VALUE;
-		int maxVal = Integer.MIN_VALUE;
-		for(int i=0;i<N;i++) {
-			if(arr[i] < minVal) {
-				minVal = arr[i];
-			}
-			if(arr[i] > maxVal) {
-				maxVal = arr[i];
-			}
-		}
-		System.out.println(minVal + " " + maxVal);
+		System.out.println(st.countTokens());
 	}
 }
