fa09f4582c3cc56dbbc82cf99132b40e124a90f3
akak4456 <akak4456@naver.com>
2025-01-05 11:46:09 +0900

    [CNG] 11720번: 숫자의 합 풀었음

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		int sum = 0;
 		for(int i=0;i<N;i++) {
-			for(int j=0;j<N-i-1;j++) {
-				System.out.print(" ");
-			}
-			for(int j=0;j<=i;j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+			sum += s.charAt(i) - '0';
 		}
+		System.out.println(sum);
 	}
 }
