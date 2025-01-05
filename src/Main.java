3553cd7f5e7d45093f44db07f73613078716ca49
akak4456 <akak4456@naver.com>
2025-01-05 11:38:44 +0900

    [CNG] 11654번: 아스키 코드 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
-	private static int A,B;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-		System.out.println(A * B);
+		String s = br.readLine();
+		int ch = s.charAt(0);
+		System.out.println(ch);
 	}
 }
