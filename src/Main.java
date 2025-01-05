c0b0080d92dc21f42b51656f6e5559ceec27fe5b
akak4456 <akak4456@naver.com>
2025-01-05 12:07:52 +0900

    [CNG] 2577번: 숫자의 개수 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
-	private static int N;
+	private static int A, B, C;
 	private static int arr[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		System.out.println(st.countTokens());
+		A = Integer.parseInt(br.readLine());
+		B = Integer.parseInt(br.readLine());
+		C = Integer.parseInt(br.readLine());
+		String result = A * B * C + "";
+		arr = new int[10];
+		for(int i=0;i<result.length();i++) {
+			arr[result.charAt(i) - '0']++;
+		}
+		for(int i=0;i<10;i++) {
+			System.out.println(arr[i]);
+		}
 	}
 }
