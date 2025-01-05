c1a7a2c387f0b24d17e4b755782441fbd718f3ed
akak4456 <akak4456@naver.com>
2025-01-05 11:51:28 +0900

    [CNG] 31403번: A + B - C 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		String s = br.readLine();
-		int sum = 0;
-		for(int i=0;i<N;i++) {
-			sum += s.charAt(i) - '0';
-		}
-		System.out.println(sum);
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		System.out.println(A + B - C);
+		String AsumB = A + "" + B;
+		System.out.println(Integer.parseInt(AsumB) - C) ;
 	}
 }
