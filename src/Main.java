9ded21a432da57cd972a78eb13541d526e17f17d
akak4456 <akak4456@naver.com>
2025-01-05 11:36:23 +0900

    [CNG] 10998번: AxB 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
+	private static int A,B;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int A, B;
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			if(A == 0 && B == 0) break;
-			System.out.println(A+B);
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		System.out.println(A * B);
 	}
 }
