f3a501121c65aee839b755294bb8e3d76ee5a454
akak4456 <akak4456@naver.com>
2025-01-05 12:00:55 +0900

    [CNG] 10250번: ACM 호텔 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
-	private static int H, M;
+	private static int T;
+	private static int H, W, N;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		H = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		M -= 45;
-		if(M < 0) {
-			M += 60;
-			H -= 1;
+		T = Integer.parseInt(br.readLine());
+		for(int testCase = 0; testCase < T; testCase++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			H = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			int remainH = N;
+			int step = 1;
+			while(remainH > H) {
+				remainH -= H;
+				step++;
+			}
+			if(step < 10) {
+				System.out.println(remainH + "0" + step);
+			}else{
+				System.out.println(remainH + "" + step);
+			}
 		}
-		if(H < 0) {
-			H += 24;
-		}
-		System.out.println(H + " " + M);
 	}
 }
