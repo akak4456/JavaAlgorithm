2a10d08c76f886910d7eec93a2a5c1f45055993c
akak4456 <akak4456@naver.com>
2025-01-05 11:53:58 +0900

    [CNG] 2562번: 최댓값 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
+	private static int arr[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
-		int C = Integer.parseInt(br.readLine());
-		System.out.println(A + B - C);
-		String AsumB = A + "" + B;
-		System.out.println(Integer.parseInt(AsumB) - C) ;
+		arr = new int[9];
+		for(int i=0;i<9;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		int maxVal = 0;
+		int maxIdx = -1;
+		for(int i=0;i<9;i++) {
+			if(arr[i] > maxVal) {
+				maxVal = arr[i];
+				maxIdx = i;
+			}
+		}
+		System.out.println(maxVal);
+		System.out.println(maxIdx + 1);
 	}
 }
