42522f84ee07d4ff48c8de7ff8612576c4bd72d3
akak4456 <akak4456@naver.com>
2025-01-11 09:30:00 +0900

    [CNG] 8958번: OX퀴즈 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 public class Main {
-	private static int arr[];
+	private static int T;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		arr = new int[42];
-		for(int i = 0; i < 10; i++) {
-			int a = Integer.parseInt(br.readLine());
-			arr[a % 42]++;
-		}
-		int cnt = 0;
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] > 0) {
-				cnt++;
+		T = Integer.parseInt(br.readLine());
+		for(int testCase = 0; testCase < T; testCase++) {
+			String line = br.readLine();
+			int scores = 0;
+			int score = 0;
+			for(int i=0;i<line.length();i++) {
+				if(line.charAt(i) == 'O') {
+					score++;
+				} else {
+					score = 0;
+				}
+				scores += score;
 			}
+			System.out.println(scores);
 		}
-		System.out.println(cnt);
 	}
 }
