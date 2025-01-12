fa1010fe4523ad269ad57d01fa452bfbe4229887
akak4456 <akak4456@naver.com>
2025-01-12 11:33:40 +0900

    [CNG] 4153번: 직각삼각형 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+
 public class Main {
-	private static int pos[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		pos = new int[26];
-		for(int i=0;i<26;i++) {
-			pos[i] = -1;
-		}
-		String line = br.readLine();
-		for(int i=0;i<line.length();i++) {
-			int idx = line.charAt(i) - 'a';
-			if(pos[idx] == -1) {
-				pos[idx] = i;
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			if(a == 0 && b == 0 && c == 0) break;
+			if(c < a) {
+				int tmp = a;
+				a = c;
+				c = tmp;
+			}
+			if(c < b) {
+				int tmp = b;
+				b = c;
+				c = tmp;
+			}
+			if(a * a + b * b == c * c) {
+				System.out.println("right");
+			} else {
+				System.out.println("wrong");
 			}
 		}
-		for(int i=0;i<26;i++) {
-			System.out.print(pos[i] + " ");
-		}
-		System.out.println();
 	}
 }
