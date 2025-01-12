44f12b319155148fb1aa74bd7a75ecd1d7606cb2
akak4456 <akak4456@naver.com>
2025-01-12 11:44:19 +0900

    [CNG] 30802번: 웰컴 키트 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int N;
+	private static int S, M, L, XL, XXL, XXXL;
+	private static int T, P;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			if(a == 0 && b == 0 && c == 0) break;
-			if(c < a) {
-				int tmp = a;
-				a = c;
-				c = tmp;
-			}
-			if(c < b) {
-				int tmp = b;
-				b = c;
-				c = tmp;
-			}
-			if(a * a + b * b == c * c) {
-				System.out.println("right");
-			} else {
-				System.out.println("wrong");
-			}
-		}
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		S = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		XL = Integer.parseInt(st.nextToken());
+		XXL = Integer.parseInt(st.nextToken());
+		XXXL = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine(), " ");
+		T = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		int shirtCnt = 0;
+		shirtCnt += (int)Math.ceil(S * 1.0 / T);
+		shirtCnt += (int)Math.ceil(M * 1.0 / T);
+		shirtCnt += (int)Math.ceil(L * 1.0 / T);
+		shirtCnt += (int)Math.ceil(XL * 1.0 / T);
+		shirtCnt += (int)Math.ceil(XXL * 1.0 / T);
+		shirtCnt += (int)Math.ceil(XXXL * 1.0 / T);
+		System.out.println(shirtCnt);
+		System.out.println(N / P + " " + N % P);
 	}
 }
