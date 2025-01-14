7bab96587cf79f8473b8e0a96e53a266b313fd78
akak4456 <akak4456@naver.com>
2025-01-14 20:44:00 +0900

    [CNG] 2798번: 블랙잭 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N;
+	private static int N, M;
+	private static int arr[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		int step = 1;
-		int lastNum = 1;
-		while(N > lastNum) {
-			lastNum += 6 * step;
-			step++;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(step);
+		int minDiff = Integer.MAX_VALUE;
+		int result = -1;
+		for(int i=0;i<N;i++) {
+			for(int j=i+1;j<N;j++) {
+				for(int k=j+1;k<N;k++){
+					if(arr[i] + arr[j] + arr[k] <= M) {
+						int diff = Math.abs(M - (arr[i] + arr[j] + arr[k]));
+						if (diff < minDiff) {
+							minDiff = diff;
+							result = arr[i] + arr[j] + arr[k];
+						}
+					}
+				}
+			}
+		}
+		System.out.println(result);
 	}
 }
