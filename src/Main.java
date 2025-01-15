efa027ea54b841f7310115c5f2f4a583f2b9231c
akak4456 <akak4456@naver.com>
2025-01-15 19:33:36 +0900

    [CNG] 15829번: Hashing 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, M;
-	private static int arr[];
+	private static int L;
+	private static String line;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int minDiff = Integer.MAX_VALUE;
-		int result = -1;
-		for(int i=0;i<N;i++) {
-			for(int j=i+1;j<N;j++) {
-				for(int k=j+1;k<N;k++){
-					if(arr[i] + arr[j] + arr[k] <= M) {
-						int diff = Math.abs(M - (arr[i] + arr[j] + arr[k]));
-						if (diff < minDiff) {
-							minDiff = diff;
-							result = arr[i] + arr[j] + arr[k];
-						}
-					}
-				}
-			}
+		L = Integer.parseInt(br.readLine());
+		line = br.readLine();
+		long result = 0;
+		long p = 1;
+		final int M = 1234567891;
+		for(int i=0;i<L;i++) {
+			result = (result + (line.charAt(i) - 'a' + 1) * p) % M;
+			p = (p * 31) % M;
 		}
 		System.out.println(result);
 	}
