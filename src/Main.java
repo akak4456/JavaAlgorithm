236b918d98a087f01c921bef81db52e272ce5534
akak4456 <akak4456@naver.com>
2025-01-16 17:42:00 +0900

    [CNG] 2869번: 달팽이는 올라가고 싶다 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int T;
-	private static int k,n;
-	private static int board[][];
+	private static int A, B, V;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		board = new int[14 + 1][14 + 1];
-		T = Integer.parseInt(br.readLine());
-		for(int i=1;i<=14;i++) {
-			board[0][i] = i;
-		}
-		for(int i=1;i<=14;i++) {
-			for(int j=1;j<=14;j++) {
-				int sum = 0;
-				for(int k=1;k<=j;k++) {
-					sum += board[i-1][k];
-				}
-				board[i][j] = sum;
-			}
-		}
-		for (int i = 0; i < T; i++) {
-			k = Integer.parseInt(br.readLine());
-			n = Integer.parseInt(br.readLine());
-			System.out.println(board[k][n]);
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
+		int days = (int)Math.ceil((double)(V - A) / (A - B));
+		System.out.println(days + 1);
 	}
 }
