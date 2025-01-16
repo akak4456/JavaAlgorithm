db49e8bb700a1762a0039c0c8d3651ca50d0f7ee
akak4456 <akak4456@naver.com>
2025-01-16 17:46:16 +0900

    [CNG] 10989번: 수 정렬하기 3 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int A, B, V;
+	private static int N;
+	private static int[] arr;
 	public static void main(String[] args) throws Exception {
+		// 입력 최적화를 위해서 Scanner 대신에 BufferedReader, StringTokenizer 를
+		// 혼합하는 방식으로 사용함
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-		V = Integer.parseInt(st.nextToken());
-		int days = (int)Math.ceil((double)(V - A) / (A - B));
-		System.out.println(days + 1);
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		StringBuilder sb = new StringBuilder();
+		Arrays.sort(arr);
+		for(int i=0;i<N;i++) {
+			sb.append(arr[i]);
+			sb.append("\n");
+		}
+		System.out.println(sb);
 	}
 }
