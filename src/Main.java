844b0ea3a8575a5437c20e4384df1ccca4b99fcd
akak4456 <akak4456@naver.com>
2025-01-21 16:12:57 +0900

    [CNG] 11650번: 좌표 정렬하기 풀었음:

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,41 @@
 import java.io.*;
 import java.util.*;
 
-class IntKeyPair<V> implements Comparable<IntKeyPair<V>> {
-	private int first;
-	private V second;
+class MainPair implements Comparable<MainPair> {
+	int x;
+	int y;
 
-	public IntKeyPair(int first, V second) {
-		this.first = first;
-		this.second = second;
-	}
-
-	public int getFirst() {
-		return first;
-	}
-
-	public V getSecond() {
-		return second;
+	public MainPair(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 
 	@Override
-	public int compareTo(IntKeyPair<V> o) {
-		return Integer.compare(this.first, o.first);
+	public int compareTo(MainPair o) {
+		if(this.x == o.x) {
+			return Integer.compare(this.y, o.y);
+		}
+		return Integer.compare(this.x, o.x);
 	}
 
 }
 
 public class Main {
 	private static int N;
-	private static IntKeyPair<String>[] arr;
+	private static MainPair[] arr;
 	public static void main(String[] args) throws Exception {
 		// 입력 최적화를 위해서 Scanner 대신에 BufferedReader, StringTokenizer 를
 		// 혼합하는 방식으로 사용함
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new IntKeyPair[N];
+		arr = new MainPair[N];
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine()," ");
-			arr[i] = new IntKeyPair<String>(Integer.parseInt(st.nextToken()), st.nextToken());
+			arr[i] = new MainPair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 		}
 		Arrays.sort(arr);
 		for(int i=0;i<N;i++) {
-			System.out.println(arr[i].getFirst() + " " + arr[i].getSecond());
+			System.out.println(arr[i].x + " " + arr[i].y);
 		}
 	}
 }
