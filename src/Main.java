95415f5621cf86039cf80efdcbfcc70c99f4dfe4
akak4456 <akak4456@naver.com>
2025-01-21 16:09:17 +0900

    [CNG] 10814번: 나이순 정렬 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,46 @@
 import java.io.*;
 import java.util.*;
 
+class IntKeyPair<V> implements Comparable<IntKeyPair<V>> {
+	private int first;
+	private V second;
+
+	public IntKeyPair(int first, V second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public int getFirst() {
+		return first;
+	}
+
+	public V getSecond() {
+		return second;
+	}
+
+	@Override
+	public int compareTo(IntKeyPair<V> o) {
+		return Integer.compare(this.first, o.first);
+	}
+
+}
+
 public class Main {
 	private static int N;
-	private static int x[];
-	private static int y[];
+	private static IntKeyPair<String>[] arr;
 	public static void main(String[] args) throws Exception {
+		// 입력 최적화를 위해서 Scanner 대신에 BufferedReader, StringTokenizer 를
+		// 혼합하는 방식으로 사용함
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		x = new int[N];
-		y = new int[N];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			x[i] = Integer.parseInt(st.nextToken());
-			y[i] = Integer.parseInt(st.nextToken());
-		}
-		StringBuilder sb = new StringBuilder();
+		arr = new IntKeyPair[N];
 		for(int i=0;i<N;i++) {
-			int curRank = 1;
-			for(int j=0;j<N;j++) {
-				if(i == j) continue;
-				if(x[j] > x[i] && y[j] > y[i]) {
-					curRank++;
-				}
-			}
-			sb.append(curRank);
-			sb.append(" ");
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			arr[i] = new IntKeyPair<String>(Integer.parseInt(st.nextToken()), st.nextToken());
 		}
-		System.out.println(sb);
+		Arrays.sort(arr);
+		for(int i=0;i<N;i++) {
+			System.out.println(arr[i].getFirst() + " " + arr[i].getSecond());
+		}
 	}
 }
