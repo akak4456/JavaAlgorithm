fe5b81a90b23d291d387f68e0faa4aa7dc05934a
akak4456 <akak4456@naver.com>
2025-01-22 15:12:18 +0900

    [CNG] 2839번 설탕 배달 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,25 @@ import java.util.*;
 
 public class Main {
 	private static int N;
-	private static Deque<Integer> deque;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		deque = new ArrayDeque<>();
-		for(int i=1;i<=N;i++) {
-			deque.addLast(i);
-		}
-		while(deque.size() > 1) {
-			deque.removeFirst();
-			if(deque.size() == 1) {
-				break;
+		int result = Integer.MAX_VALUE;
+		int fiveCnt = 0;
+		while(N > 0) {
+			if(N % 3 == 0) {
+				result = Math.min(result, fiveCnt + N / 3);
 			}
-			int a = deque.removeFirst();
-			deque.addLast(a);
+			N -= 5;
+			fiveCnt++;
 		}
-		System.out.println(deque.getFirst());
+		if(N == 0) {
+			result = Math.min(result, fiveCnt);
+		}
+		if(result == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		} else {
+			System.out.println(result);
+		}
 	}
 }
