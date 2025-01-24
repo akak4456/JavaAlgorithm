8a6d1f0b1fdf489f3be41441d9481ed77802a82a
akak4456 <akak4456@naver.com>
2025-01-24 14:43:22 +0900

    [CNG] 10828번: 스택 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, M;
-	private static Map<Integer, Integer> map;
+	private static int N;
+	private static Stack<Integer> stack;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		map = new HashMap<>();
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		stack = new Stack<>();
 		for(int i=0;i<N;i++) {
-			int a = Integer.parseInt(st.nextToken());
-			if(map.containsKey(a)) {
-				map.put(a, map.get(a) + 1);
-			} else {
-				map.put(a, 1);
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			switch(st.nextToken()) {
+				case "push":
+					stack.push(Integer.parseInt(st.nextToken()));
+					break;
+				case "pop":
+					if(stack.isEmpty()) {
+						System.out.println(-1);
+					} else {
+						System.out.println(stack.pop());
+					}
+					break;
+				case "size":
+					System.out.println(stack.size());
+					break;
+				case "empty":
+					System.out.println(stack.isEmpty() ? 1 : 0);
+					break;
+				case "top":
+					if(stack.isEmpty()) {
+						System.out.println(-1);
+					} else {
+						System.out.println(stack.peek());
+					}
+					break;
 			}
 		}
-		M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine(), " ");
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<M;i++) {
-			int a = Integer.parseInt(st.nextToken());
-			if(map.containsKey(a)) {
-				sb.append(map.get(a)).append(" ");
-			} else {
-				sb.append("0 ");
-			}
-		}
-		System.out.println(sb);
 	}
 }
