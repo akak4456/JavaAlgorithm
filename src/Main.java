9f77819e252751bc9db6e252e93382b0441c0224
akak4456 <akak4456@naver.com>
2025-01-24 14:41:17 +0900

    [CNG] 10816번: 숫자 카드 2 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int K;
-	private static Stack<Integer> st;
+	private static int N, M;
+	private static Map<Integer, Integer> map;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		K = Integer.parseInt(br.readLine());
-		st = new Stack<>();
-		for(int i=0;i<K;i++) {
-			int t = Integer.parseInt(br.readLine());
-			if(t == 0) {
-				st.pop();
+		N = Integer.parseInt(br.readLine());
+		map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0;i<N;i++) {
+			int a = Integer.parseInt(st.nextToken());
+			if(map.containsKey(a)) {
+				map.put(a, map.get(a) + 1);
 			} else {
-				st.push(t);
+				map.put(a, 1);
 			}
 		}
-		int sum = 0;
-		while(!st.isEmpty()) {
-			sum += st.pop();
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<M;i++) {
+			int a = Integer.parseInt(st.nextToken());
+			if(map.containsKey(a)) {
+				sb.append(map.get(a)).append(" ");
+			} else {
+				sb.append("0 ");
+			}
 		}
-		System.out.println(sum);
+		System.out.println(sb);
 	}
 }
