1d09414e1951be910aca6fb33ceb604513428d66
akak4456 <akak4456@naver.com>
2025-01-25 12:16:02 +0900

    [CNG] 11866번: 요세푸스 문제 0 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N;
-	private static Deque<Integer> queue;
+	private static int N, K;
+	private static Deque<Integer> deque;
+	private static ArrayList<Integer> ans;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		queue = new LinkedList<>();
-		for(int i=0;i<N;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
-			switch(st.nextToken()) {
-				case "push":
-					queue.add(Integer.parseInt(st.nextToken()));
-					break;
-				case "pop":
-					if(queue.isEmpty()) {
-						System.out.println(-1);
-					} else {
-						System.out.println(queue.poll());
-					}
-					break;
-				case "size":
-					System.out.println(queue.size());
-					break;
-				case "empty":
-					System.out.println(queue.isEmpty() ? 1 : 0);
-					break;
-				case "front":
-					if(queue.isEmpty()) {
-						System.out.println(-1);
-					} else {
-						System.out.println(queue.peek());
-					}
-					break;
-				case "back":
-					if(queue.isEmpty()) {
-						System.out.println(-1);
-					} else {
-						System.out.println(queue.getLast());
-					}
-					break;
-			}
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		deque = new ArrayDeque<>();
+		ans = new ArrayList<>();
+		for(int i = 1; i <= N; i++) {
+			deque.addLast(i);
 		}
+		while(!deque.isEmpty()) {
+			for(int i=1;i<=K-1;i++) {
+				deque.addLast(deque.removeFirst());
+			}
+			ans.add(deque.removeFirst());
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		for(int i=0;i<ans.size()-1;i++) {
+			sb.append(ans.get(i)).append(", ");
+		}
+		sb.append(ans.get(ans.size()-1)).append(">");
+		System.out.println(sb);
 	}
 }
