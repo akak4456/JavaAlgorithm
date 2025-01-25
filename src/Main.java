228fc96202c073a580b8ebc8687ac0862d4f18ad
akak4456 <akak4456@naver.com>
2025-01-25 12:33:35 +0900

    [CNG] 1929번: 소수 구하기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int n;
-	private static int arr[];
+	private static boolean isPrime[];
+	private static int M, N;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n];
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		isPrime = new boolean[1000000+1];
+		Arrays.fill(isPrime, true);
+		isPrime[0] = false;
+		isPrime[1] = false;
+		for(int i=2;i<=1000000;i++) {
+			if(isPrime[i]) {
+				for(int j=i*2;j<=1000000;j+=i) {
+					isPrime[j] = false;
+				}
+			}
 		}
-		Arrays.sort(arr);
-		int cutCount = (int)Math.round((double)n * 0.15);
-		int sum = 0;
-		for(int i=cutCount;i<n-cutCount;i++) {
-			sum += arr[i];
+		StringBuilder sb = new StringBuilder();
+		for(int i=M;i<=N;i++) {
+			if(isPrime[i]) {
+				sb.append(i).append("\n");
+			}
 		}
-		int ans = (int)Math.round((double) sum / (n - cutCount * 2));
-		System.out.println(ans);
+		System.out.println(sb);
 	}
 }
