d27c56e06de40a6b577d23d9e16fd5ea74bc6882
akak4456 <akak4456@naver.com>
2025-01-29 20:36:00 +0900

    [CNG] 1966번: 프린터 큐 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static boolean isPrime[];
-	private static int M, N;
+	private static int T;
+	private static int N, M;
+	private static int[] priority;
+	private static int[] queue;
+	private static int queueFirstPointer;
+	private static int queueLastPointer;
+	private static int[] result;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		isPrime = new boolean[1000000+1];
-		Arrays.fill(isPrime, true);
-		isPrime[0] = false;
-		isPrime[1] = false;
-		for(int i=2;i<=1000000;i++) {
-			if(isPrime[i]) {
-				for(int j=i*2;j<=1000000;j+=i) {
-					isPrime[j] = false;
+		T = Integer.parseInt(br.readLine());
+		for(int testCase = 0; testCase < T; testCase++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			priority = new int[N];
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int i = 0; i < N; i++) {
+				priority[i] = Integer.parseInt(st.nextToken());
+			}
+			queue = new int[N * 10];
+			queueFirstPointer = 0;
+			queueLastPointer = N;
+			for(int i = 0; i < N; i++) {
+				queue[i] = i;
+			}
+			result = new int[N];
+			int order = 1;
+			while(queueFirstPointer < queueLastPointer) {
+				boolean isPossible = true;
+				for(int i = queueFirstPointer + 1; i < queueLastPointer; i++) {
+					if(priority[queue[i]] > priority[queue[queueFirstPointer]]) {
+						isPossible = false;
+						break;
+					}
+				}
+				if(isPossible) {
+					result[queue[queueFirstPointer]] = order++;
+					queueFirstPointer++;
+				}  else {
+					queue[queueLastPointer] = queue[queueFirstPointer];
+					queueFirstPointer++;
+					queueLastPointer++;
 				}
 			}
+			System.out.println(result[M]);
 		}
-		StringBuilder sb = new StringBuilder();
-		for(int i=M;i<=N;i++) {
-			if(isPrime[i]) {
-				sb.append(i).append("\n");
-			}
-		}
-		System.out.println(sb);
 	}
 }
