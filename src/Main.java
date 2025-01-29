9afe56305cc1c315016155f1192e721d6147b808
akak4456 <akak4456@naver.com>
2025-01-29 20:53:19 +0900

    [CNG] 1874번: 스택 수열 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int K, N;
-	private static int[] arr;
+	private static int n;
+	private static int arr[];
+	private static ArrayList<Character> op;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		K = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		arr = new int[K];
-		for(int i = 0; i < K; i++) {
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-		long start = 0;
-		long end = Integer.MAX_VALUE;
-		long result = 0;
-		while(start <= end) {
-			long mid = (start + end) / 2;
-			int cnt = 0;
-			for(int i=0;i<K;i++) {
-				cnt += arr[i] / mid;
+		Stack<Integer> stack = new Stack<>();
+		int arrPointer = 0;
+		op = new ArrayList<>();
+		for(int i=1;i<=n;i++) {
+			if(stack.isEmpty() || stack.peek() != arr[arrPointer]) {
+				stack.push(i);
+				op.add('+');
 			}
-			if(cnt >= N) {
-				result = mid;
-				start = mid + 1;
-			} else {
-				end = mid - 1;
+			while(arrPointer < n && !stack.isEmpty() && stack.peek() == arr[arrPointer]) {
+				op.add('-');
+				arrPointer++;
+				stack.pop();
 			}
 		}
-		System.out.println(result);
+		if(arrPointer == n) {
+			StringBuilder sb = new StringBuilder();
+            for (Character character : op) {
+                sb.append(character);
+                sb.append("\n");
+            }
+			System.out.println(sb);
+		} else {
+			System.out.println("NO");
+		}
 	}
 }
