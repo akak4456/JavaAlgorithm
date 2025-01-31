95a1db9bf666ea6392a65b6e9d7a685595fb97b5
akak4456 <akak4456@naver.com>
2025-02-01 05:10:57 +0900

    [CNG] 11723번 집합 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int n;
-	private static int arr[];
-	private static ArrayList<Character> op;
-	public static void main(String[] args) throws Exception {
+	private static int M;
+	private static int mask;
+	private static int allMask;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=1;i<=20;i++) {
+			allMask |= (1<<i);
 		}
-		Stack<Integer> stack = new Stack<>();
-		int arrPointer = 0;
-		op = new ArrayList<>();
-		for(int i=1;i<=n;i++) {
-			if(stack.isEmpty() || stack.peek() != arr[arrPointer]) {
-				stack.push(i);
-				op.add('+');
+		for(int i=0;i<M;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			String op = st.nextToken();
+			switch(op) {
+				case "add":
+					int operand = Integer.parseInt(st.nextToken());
+					mask |= (1 << operand);
+					break;
+				case "remove":
+					operand = Integer.parseInt(st.nextToken());
+					mask &= ~(1 << operand);
+					break;
+				case "check":
+					operand = Integer.parseInt(st.nextToken());
+					boolean isChecked = (mask & (1 << operand)) > 0;
+					if(isChecked) {
+						sb.append("1\n");
+					} else {
+						sb.append("0\n");
+					}
+					break;
+				case "toggle":
+					operand = Integer.parseInt(st.nextToken());
+					mask ^= (1 << operand);
+					break;
+				case "all":
+					mask = allMask;
+					break;
+				case "empty":
+					mask = 0;
+					break;
 			}
-			while(arrPointer < n && !stack.isEmpty() && stack.peek() == arr[arrPointer]) {
-				op.add('-');
-				arrPointer++;
-				stack.pop();
-			}
 		}
-		if(arrPointer == n) {
-			StringBuilder sb = new StringBuilder();
-            for (Character character : op) {
-                sb.append(character);
-                sb.append("\n");
-            }
-			System.out.println(sb);
-		} else {
-			System.out.println("NO");
-		}
+		System.out.println(sb);
 	}
 }
