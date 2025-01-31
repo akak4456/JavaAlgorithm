1c597cbd4657f10d89767d1492df008ad91764f6
akak4456 <akak4456@naver.com>
2025-02-01 05:20:52 +0900

    [CNG] 9012번 괄호 출었음

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int M;
-	private static int mask;
-	private static int allMask;
+	private static int T;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		M = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for(int i=1;i<=20;i++) {
-			allMask |= (1<<i);
-		}
-		for(int i=0;i<M;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			String op = st.nextToken();
-			switch(op) {
-				case "add":
-					int operand = Integer.parseInt(st.nextToken());
-					mask |= (1 << operand);
-					break;
-				case "remove":
-					operand = Integer.parseInt(st.nextToken());
-					mask &= ~(1 << operand);
-					break;
-				case "check":
-					operand = Integer.parseInt(st.nextToken());
-					boolean isChecked = (mask & (1 << operand)) > 0;
-					if(isChecked) {
-						sb.append("1\n");
-					} else {
-						sb.append("0\n");
+		T = Integer.parseInt(br.readLine());
+		for(int testCase = 0; testCase < T; testCase++) {
+			String line = br.readLine();
+			Stack<Character> st = new Stack<>();
+			boolean isPossible = true;
+			for(int i = 0; i < line.length(); i++) {
+				if(line.charAt(i) == '(') {
+					st.push(line.charAt(i));
+				}
+				if(line.charAt(i) == ')') {
+					if(st.isEmpty()) {
+						isPossible = false;
+						break;
 					}
-					break;
-				case "toggle":
-					operand = Integer.parseInt(st.nextToken());
-					mask ^= (1 << operand);
-					break;
-				case "all":
-					mask = allMask;
-					break;
-				case "empty":
-					mask = 0;
-					break;
+					st.pop();
+				}
+			}
+			if(!st.isEmpty()) {
+				isPossible = false;
+			}
+			if(isPossible) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
 			}
 		}
-		System.out.println(sb);
 	}
 }
