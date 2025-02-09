2fbc6bec9e985e61fd73c5df2ad9d23d126a4324
akak4456 <akak4456@naver.com>
2025-02-09 11:39:37 +0900

    [CNG] 11399번: ATM 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,23 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-	private static int N, K;
+	private static int N;
 	private static int arr[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
 		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		int cnt = 0;
-		for(int i=N-1;i>=0;i--) {
-			while(K >= arr[i]) {
-				K -= arr[i];
-				cnt++;
-			}
+		int result = 0;
+		int cum = 0;
+		Arrays.sort(arr);
+		for(int i=0;i<N;i++) {
+			cum += arr[i];
+			result += cum;
 		}
-		System.out.println(cnt);
+		System.out.println(result);
 	}
 }
