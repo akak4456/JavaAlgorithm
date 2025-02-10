7a28e56f491c85696d0fb6ad1a84c34d4e07a97a
akak4456 <akak4456@naver.com>
2025-02-10 10:50:11 +0900

    [CNG] 1003번: 피보나치 함수 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,27 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-	private static int N, M;
-	private static Map<String, String> map;
+	private static int T;
+	private static int N;
+	private static int[] dp0;
+	private static int[] dp1;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new HashMap<>();
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			map.put(st.nextToken(), st.nextToken());
+		T = Integer.parseInt(br.readLine());
+		dp0 = new int[40 + 1];
+		dp1 = new int[40 + 1];
+		dp0[0] = 1;
+		dp1[0] = 0;
+		dp0[1] = 0;
+		dp1[1] = 1;
+		for(int i=2;i<=40;i++) {
+			dp0[i] = dp0[i-1] + dp0[i-2];
+			dp1[i] = dp1[i-1] + dp1[i-2];
 		}
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < M; i++) {
-			String line = br.readLine();
-			sb.append(map.get(line)).append("\n");
+		for(int testCase = 0; testCase < T; testCase++) {
+			N = Integer.parseInt(br.readLine());
+			sb.append(dp0[N]).append(" ").append(dp1[N]).append("\n");
 		}
 		System.out.println(sb);
 	}
