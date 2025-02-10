120a4c4288daf22706c5583e8c376d6f9d315e83
akak4456 <akak4456@naver.com>
2025-02-10 10:54:10 +0900

    [CNG] 1463번 1로 만들기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,23 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-	private static int T;
+
 	private static int N;
-	private static int[] dp0;
-	private static int[] dp1;
+	private static int[] dp;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
-		dp0 = new int[40 + 1];
-		dp1 = new int[40 + 1];
-		dp0[0] = 1;
-		dp1[0] = 0;
-		dp0[1] = 0;
-		dp1[1] = 1;
-		for(int i=2;i<=40;i++) {
-			dp0[i] = dp0[i-1] + dp0[i-2];
-			dp1[i] = dp1[i-1] + dp1[i-2];
+		N = Integer.parseInt(br.readLine());
+		dp = new int[1000000 + 1];
+		dp[1] = 0;
+		for(int i=2;i<=N;i++) {
+			dp[i] = dp[i-1] + 1; // 1을 빼는 경우의 수
+			if(i >= 2 && i%2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2] + 1); // 2로 나누는 경우의 수
+			}
+			if(i >= 3 && i%3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/3] + 1); // 3로 나누는 경우의 수
+			}
 		}
-		StringBuilder sb = new StringBuilder();
-		for(int testCase = 0; testCase < T; testCase++) {
-			N = Integer.parseInt(br.readLine());
-			sb.append(dp0[N]).append(" ").append(dp1[N]).append("\n");
-		}
-		System.out.println(sb);
+		System.out.println(dp[N]);
 	}
 }
