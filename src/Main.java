734d198c5f2f081bbef9b8c927bc2280979dfb06
akak4456 <akak4456@naver.com>
2025-02-11 16:38:06 +0900

    [CNG] 2606번 바이러스 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,33 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-	private static int N;
-	private static int[] arr;
-	private static int[][] dp;
-	private static int solve(int curPos, int curStep) {
-		if(dp[curPos][curStep] != -1) return dp[curPos][curStep];
-		if (curStep == 2 && curPos == N - 1) {
-			return Integer.MIN_VALUE;
+	private static int N, M;
+	private static int adj[][];
+	private static boolean visited[];
+	private static int cnt;
+	private static void dfs(int node) {
+		visited[node] = true;
+		cnt++;
+		for(int i=1; i<=N;i++) {
+			if(!visited[i] && adj[node][i] == 1) {
+				dfs(i);
+			}
 		}
-		if(curPos == N) return 0;
-		int ret = 0;
-		if(curStep < 2 && curPos + 1 <= N) {
-			ret = Math.max(ret, solve(curPos + 1, curStep + 1) + arr[curPos + 1]);
-		}
-		if(curPos + 2 <= N) {
-			ret = Math.max(ret, solve(curPos + 2, 1) + arr[curPos + 2]);
-		}
-		dp[curPos][curStep] = ret;
-		return ret;
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N + 1];
-		dp = new int[N + 1][3];
-		for(int i=0;i<=N;i++) {
-			Arrays.fill(dp[i], -1);
+		M = Integer.parseInt(br.readLine());
+		adj = new int[N + 1][N + 1];
+		visited = new boolean[N + 1];
+		for(int i=0;i<M;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adj[a][b] = 1;
+			adj[b][a] = 1;
 		}
-		for(int i=1;i<=N;i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		System.out.println(solve(0,0));
+		dfs(1);
+		System.out.println(cnt - 1); // 1번 컴퓨터는 빼야 하므로
 	}
 }
