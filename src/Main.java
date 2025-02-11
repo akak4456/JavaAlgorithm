9510212c260ab27cc2b42df3b172a8336e9ee1cc
akak4456 <akak4456@naver.com>
2025-02-11 16:33:52 +0900

    [CNG] 2579번: 계단 오르기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,36 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-
 	private static int N;
-	private static int[] dp;
+	private static int[] arr;
+	private static int[][] dp;
+	private static int solve(int curPos, int curStep) {
+		if(dp[curPos][curStep] != -1) return dp[curPos][curStep];
+		if (curStep == 2 && curPos == N - 1) {
+			return Integer.MIN_VALUE;
+		}
+		if(curPos == N) return 0;
+		int ret = 0;
+		if(curStep < 2 && curPos + 1 <= N) {
+			ret = Math.max(ret, solve(curPos + 1, curStep + 1) + arr[curPos + 1]);
+		}
+		if(curPos + 2 <= N) {
+			ret = Math.max(ret, solve(curPos + 2, 1) + arr[curPos + 2]);
+		}
+		dp[curPos][curStep] = ret;
+		return ret;
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		dp = new int[1000000 + 1];
-		dp[1] = 0;
-		for(int i=2;i<=N;i++) {
-			dp[i] = dp[i-1] + 1; // 1을 빼는 경우의 수
-			if(i >= 2 && i%2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/2] + 1); // 2로 나누는 경우의 수
-			}
-			if(i >= 3 && i%3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/3] + 1); // 3로 나누는 경우의 수
-			}
+		arr = new int[N + 1];
+		dp = new int[N + 1][3];
+		for(int i=0;i<=N;i++) {
+			Arrays.fill(dp[i], -1);
 		}
-		System.out.println(dp[N]);
+		for(int i=1;i<=N;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		System.out.println(solve(0,0));
 	}
 }
