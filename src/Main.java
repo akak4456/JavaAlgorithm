b2b59e2df1e7805097be2bdbbb30375d7e14040b
akak4456 <akak4456@naver.com>
2025-02-13 17:49:28 +0900

    [CNG] 9095번: 1, 2, 3 더하기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,21 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-	private static int N, M;
-	private static int adj[][];
-	private static boolean visited[];
-	private static int cnt;
-	private static void dfs(int node) {
-		visited[node] = true;
-		cnt++;
-		for(int i=1; i<=N;i++) {
-			if(!visited[i] && adj[node][i] == 1) {
-				dfs(i);
-			}
-		}
-	}
+	private static int T;
+	private static int dp[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		adj = new int[N + 1][N + 1];
-		visited = new boolean[N + 1];
-		for(int i=0;i<M;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			adj[a][b] = 1;
-			adj[b][a] = 1;
+		T = Integer.parseInt(br.readLine());
+		dp = new int[10 + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for(int i=4;i<=10;i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
 		}
-		dfs(1);
-		System.out.println(cnt - 1); // 1번 컴퓨터는 빼야 하므로
+		for(int testCase = 0; testCase < T; testCase++) {
+			int n = Integer.parseInt(br.readLine());
+			System.out.println(dp[n]);
+		}
 	}
 }
