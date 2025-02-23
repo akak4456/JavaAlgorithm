a8b66241be6c88d0fece30c21c5bb9cf252a3d78
akak4456 <akak4456@naver.com>
2025-02-23 09:11:42 +0900

    [CNG] 9461번: 파도반 수열 풀었음

--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,21 @@ import java.util.stream.Collectors;
 
 public class Main {
 	private static int T;
-	private static int n;
-	private static int[][] dp;
+	private static int N;
+	private static long dp[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for(int testCase = 0; testCase < T; testCase++) {
-			n = Integer.parseInt(br.readLine());
-			Map<String, List<String>> m = new HashMap<>();
-			for(int i = 0; i < n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				String wear = st.nextToken();
-				String wearKind = st.nextToken();
-				m.putIfAbsent(wearKind, new ArrayList<>());
-				m.get(wearKind).add(wear);
-			}
-			List<Integer> l = new ArrayList<>();
-			for (Map.Entry<String, List<String>> entry : m.entrySet()) {
-				l.add(entry.getValue().size());
-			}
-			dp = new int[l.size()][l.size()];
-			for(int i=0;i<l.size();i++) {
-				dp[0][i] = l.get(i);
-			}
-			for(int i=1;i<l.size();i++) {
-				for(int j=0;j<l.size();j++) {
-					for(int k=j+1;k<l.size();k++) {
-						dp[i][j] += l.get(j) * dp[i-1][k];
-					}
-				}
-			}
-			int result = 0;
-
-			for(int i=0;i<l.size();i++) {
-				for(int j=0;j<l.size();j++) {
-					result += dp[i][j];
-				}
-			}
-			sb.append(result).append("\n");
+		dp = new long[100 + 1];
+		dp[1] = 1;
+		dp[2] = 1;
+		dp[3] = 1;
+		for(int i=4;i<=100;i++) {
+			dp[i] = dp[i-2] + dp[i-3];
 		}
-		System.out.println(sb);
+		for(int testCase = 0; testCase < T; testCase++) {
+			N = Integer.parseInt(br.readLine());
+			System.out.println(dp[N]);
+		}
 	}
 }
