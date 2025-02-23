d4233f7a0a22b6c99d412ad7b9f6f36075bd3aae
akak4456 <akak4456@naver.com>
2025-02-23 09:16:18 +0900

    [CNG] 11659번: 구간 합 구하기 4 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,28 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-	private static int T;
-	private static int N;
-	private static long dp[];
+	private static int N, M;
+	private static int[] arr;
+	private static int[] sum;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
-		dp = new long[100 + 1];
-		dp[1] = 1;
-		dp[2] = 1;
-		dp[3] = 1;
-		for(int i=4;i<=100;i++) {
-			dp[i] = dp[i-2] + dp[i-3];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N + 1];
+		sum = new int[N + 1];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum[i] = arr[i] + sum[i - 1];
 		}
-		for(int testCase = 0; testCase < T; testCase++) {
-			N = Integer.parseInt(br.readLine());
-			System.out.println(dp[N]);
+		StringBuilder sb = new StringBuilder();
+		for(int m = 0; m < M; m++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			sb.append(sum[j] - sum[i-1]).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
