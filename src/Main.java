5eb9f0d3d97abacc70524dcbc7f8bb12ee904df0
akak4456 <akak4456@naver.com>
2025-02-26 09:14:55 +0900

    [CNG] 11726번: 2xn 타일링 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-	private static int N, M;
-	private static int[] arr;
-	private static int[] sum;
+	private static int N;
+	private static int dp[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N + 1];
-		sum = new int[N + 1];
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			sum[i] = arr[i] + sum[i - 1];
+		N = Integer.parseInt(br.readLine());
+		dp = new int[1000 + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i=3;i<=1000;i++) {
+			dp[i] = (dp[i-1] + dp[i - 2]) % 10007;
 		}
-		StringBuilder sb = new StringBuilder();
-		for(int m = 0; m < M; m++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int i = Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
-			sb.append(sum[j] - sum[i-1]).append("\n");
-		}
-		System.out.println(sb);
+		System.out.println(dp[N]);
 	}
 }
