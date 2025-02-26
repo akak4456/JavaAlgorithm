316c9d949ffa224121358cccd4b598eb76f7ba2a
akak4456 <akak4456@naver.com>
2025-02-26 09:17:49 +0900

    [CNG] 11727번: 2xn 타일링 2 풀었음

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		dp = new int[1000 + 1];
 		dp[1] = 1;
-		dp[2] = 2;
+		dp[2] = 3;
 		for(int i=3;i<=1000;i++) {
-			dp[i] = (dp[i-1] + dp[i - 2]) % 10007;
+			dp[i] = (dp[i-1] + dp[i - 2] * 2) % 10007;
 		}
 		System.out.println(dp[N]);
 	}
