9f0e356e8736647b2151284769a0eaff30382b8b
akak4456 <akak4456@naver.com>
2025-02-28 09:01:48 +0900

    [CNG] 17626번: Four Squares 풀었음

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,16 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		dp = new int[1000 + 1];
+		dp = new int[N + 1];
+		dp[0] = 0;
 		dp[1] = 1;
-		dp[2] = 3;
-		for(int i=3;i<=1000;i++) {
-			dp[i] = (dp[i-1] + dp[i - 2] * 2) % 10007;
+		for(int i=2; i<=N; i++) {
+			dp[i] = Integer.MAX_VALUE;
+		}
+		for(int i=0;i<=N;i++) {
+			for(int j=1;i + j*j <= N;j++) {
+				dp[i + j*j] = Math.min(dp[i + j*j],dp[i] + 1);
+			}
 		}
 		System.out.println(dp[N]);
 	}
