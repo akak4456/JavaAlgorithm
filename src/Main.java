73fdf9f59aaea5b2d011ba8b0f834e77991d5964
akak4456 <akak4456@naver.com>
2025-03-02 10:08:46 +0900

    [CNG] 1927번: 최소 힙 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,23 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 public class Main {
+	private static PriorityQueue<Integer> pq;
+	private static int N;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line = br.readLine();
-		int minusIdx = -1;
-		Queue<Integer> operand = new LinkedList<>();
-		Queue<Character> opcode = new LinkedList<>();
-		int operandStartIdx = 0;
-		for (int i = 0; i < line.length(); i++) {
-			if(line.charAt(i) == '-' || line.charAt(i) == '+') {
-				operand.add(Integer.parseInt(line.substring(operandStartIdx, i)));
-				opcode.add(line.charAt(i));
-				operandStartIdx = i+1;
-			}
-			if(line.charAt(i) == '-' && minusIdx == -1) {
-				minusIdx = opcode.size() - 1;
-			}
-		}
-		operand.add(Integer.parseInt(line.substring(operandStartIdx)));
-		int result1 = 0;
-		if(minusIdx != -1) {
-			result1 = operand.poll();
-			for(int i=0;i<minusIdx;i++) {
-				result1 += operand.poll();
-				opcode.poll();
-			}
-			opcode.poll();
-		}
-		int result2 = operand.poll();
-		while(!opcode.isEmpty()) {
-			Character op = opcode.poll();
-			if(op == '+') {
-				result2 += operand.poll();
+		N = Integer.parseInt(br.readLine());
+		pq = new PriorityQueue<>();
+		for(int i=0;i<N;i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x > 0) {
+				pq.add(x);
 			} else {
-				result2 += operand.poll();
+				if(pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
 			}
-		}
-		if(minusIdx != -1) {
-			System.out.println(result1 - result2);
-		} else {
-			System.out.println(result2);
 		}
 	}
 }
