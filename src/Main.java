07ef4b61184eb47844152ba6fd351b1a7be90e53
akak4456 <akak4456@naver.com>
2025-03-04 08:41:07 +0900

    [CNG] 18111번: 마인크래프트 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,42 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 public class Main {
-	private static int N, M;
-	private static int[][] adj;
-	private static boolean[] visited;
-	private static void dfs(int node) {
-		if(visited[node]) return;
-		visited[node] = true;
-		for(int i=1;i<=N;i++) {
-			if(adj[node][i] == 1 && !visited[i]) {
-				dfs(i);
-			}
-		}
-	}
+	private static int N, M, B;
+	private static int[][] board;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		adj = new int[N + 1][N + 1];
-		visited = new boolean[N + 1];
-		for(int i=0;i<M;i++) {
+		B = Integer.parseInt(st.nextToken());
+		board = new int[N][M];
+		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			adj[a][b] = 1;
-			adj[b][a] = 1;
-		}
-		int cnt = 0;
-		for(int i=1;i<=N;i++) {
-			if(!visited[i]) {
-				cnt++;
-				dfs(i);
+			for(int j = 0; j < M; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		System.out.println(cnt);
+		int minTime = Integer.MAX_VALUE;
+		int minHeight = 0;
+		for(int targetHeight = 0; targetHeight <= 256; targetHeight++) {
+			int addBlock = 0;
+			int subtractBlock = 0;
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<M;j++) {
+					if(board[i][j] < targetHeight) {
+						addBlock += targetHeight - board[i][j];
+					} else if(board[i][j] > targetHeight) {
+						subtractBlock += board[i][j] - targetHeight;
+					}
+				}
+			}
+			if(B + subtractBlock - addBlock < 0) continue;
+			int time = addBlock + subtractBlock * 2;
+			if(time <= minTime) {
+				minTime = time;
+				minHeight = targetHeight;
+			}
+		}
+		System.out.println(minTime + " " + minHeight);
 	}
 }
