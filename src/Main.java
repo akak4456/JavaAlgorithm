a2fb55d96cb9ce1287da8f6861d7adc9a456c2b4
akak4456 <akak4456@naver.com>
2025-03-03 14:43:02 +0900

    [CNG] 11279번: 최대 힙 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,23 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 public class Main {
-	private static int N, M;
-	private static int arr[];
+	private static int N;
+	private static PriorityQueue<Integer> pq;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		long start = 0;
-		long end = 2000000000;
-
-		while(start <= end) {
-			long mid = (start + end) / 2;
-			long sum = 0;
-			for(int i=0;i<N;i++) {
-				if(arr[i] > mid) {
-					sum += arr[i] - mid;
+		N = Integer.parseInt(br.readLine());
+		pq = new PriorityQueue<>();
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				if(pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(-pq.poll());
 				}
-			}
-			if(sum >= M) {
-				start = mid + 1;
 			} else {
-				end = mid - 1;
+				pq.add(-x);
 			}
 		}
-		System.out.println(end);
 	}
 }
