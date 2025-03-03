3bce31dd50be5fdede3fbd70cec2e4f72722b8fe
akak4456 <akak4456@naver.com>
2025-03-03 14:40:34 +0900

    [CNG] 2805번: 나무 자르기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,35 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 public class Main {
-	private static int N;
-	private static int board[][];
-	private static int whiteCnt;
-	private static int blueCnt;
-	private static void solve(int startRow, int endRow, int startCol, int endCol) {
-		boolean isWhite = true;
-		for(int i=startRow; i<=endRow;i++) {
-			for(int j=startCol;j<=endCol;j++) {
-				if(board[i][j] != 0) {
-					isWhite = false;
-				}
-			}
-		}
-		if(isWhite) {
-			whiteCnt++;
-			return;
-		}
-		boolean isBlue = true;
-		for(int i=startRow; i<=endRow;i++) {
-			for(int j=startCol;j<=endCol;j++) {
-				if(board[i][j] != 1) {
-					isBlue = false;
-				}
-			}
-		}
-		if(isBlue) {
-			blueCnt++;
-			return;
-		}
-		int midRow = (startRow+endRow)/2;
-		int midCol = (startCol+endCol)/2;
-		solve(startRow, midRow, startCol, midCol);
-		solve(midRow + 1,endRow, startCol, midCol);
-		solve(startRow,midRow, midCol + 1, endCol);
-		solve(midRow+1, endRow, midCol + 1, endCol);
-	}
+	private static int N, M;
+	private static int arr[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < N; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		long start = 0;
+		long end = 2000000000;
+
+		while(start <= end) {
+			long mid = (start + end) / 2;
+			long sum = 0;
+			for(int i=0;i<N;i++) {
+				if(arr[i] > mid) {
+					sum += arr[i] - mid;
+				}
+			}
+			if(sum >= M) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
 			}
 		}
-		solve(0, N-1, 0, N-1);
-		System.out.println(whiteCnt);
-		System.out.println(blueCnt);
+		System.out.println(end);
 	}
 }
