d0188344a410ba7aaf473bee5f81ed8714c1a651
akak4456 <akak4456@naver.com>
2025-03-06 08:12:38 +0900

    [CNG] 21736번: 헌내기는 친구가 필요해 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,57 @@
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
-class P implements Comparable<P> {
-	int idx;
-	int value;
-
-	@Override
-	public int compareTo(P o) {
-		return this.value - o.value;
+class P {
+	int col;
+	int row;
+	public P(int row, int col) {
+		this.col = col;
+		this.row = row;
 	}
 }
 public class Main {
-	private static int N;
-	private static ArrayList<P> arr;
-	private static int[] result;
+	private static int N, M;
+	private static char[][] board;
+	private static int[] dcol = {0,0,-1,1};
+	private static int[] drow = {-1,1,0,0};
+	private static Queue<P> queue;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new ArrayList<>();
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		board = new char[N][M];
+		queue = new LinkedList<>();
 		for (int i = 0; i < N; i++) {
-			P p = new P();
-			p.idx = i;
-			p.value = Integer.parseInt(st.nextToken());
-			arr.add(p);
-		}
-		Collections.sort(arr);
-		result = new int[N];
-		int targetIdx = -1;
-		int targetValue = Integer.MIN_VALUE;
-		for(int i=0;i<N;i++) {
-			P p = arr.get(i);
-			if(targetValue != p.value) {
-				targetIdx++;
-				targetValue = p.value;
+			String line = br.readLine();
+			for(int j = 0; j < M; j++){
+				board[i][j] = line.charAt(j);
+				if(board[i][j] == 'I') {
+					queue.add(new P(i, j));
+				}
 			}
-			result[p.idx] = targetIdx;
 		}
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<N;i++) {
-			sb.append(result[i]).append(" ");
+		int cnt = 0;
+		while(!queue.isEmpty()) {
+			P p = queue.poll();
+			if(board[p.row][p.col] == 'V') continue;
+			if(board[p.row][p.col] == 'P') {
+				cnt++;
+			}
+			board[p.row][p.col] = 'V';
+			for(int i=0;i<4;i++) {
+				int nrow = p.row + drow[i];
+				int ncol = p.col + dcol[i];
+				if(nrow < 0 || nrow >= N || ncol < 0 || ncol >= M) continue;
+				if(board[nrow][ncol] == 'P' || board[nrow][ncol] == 'O') {
+					queue.add(new P(nrow, ncol));
+				}
+			}
 		}
-		System.out.println(sb);
+		if(cnt == 0) {
+			System.out.println("TT");
+		} else {
+			System.out.println(cnt);
+		}
 	}
 }
