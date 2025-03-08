6e7918562d582fe73a2b491fb521cee4b0961e43
akak4456 <akak4456@naver.com>
2025-03-08 10:43:37 +0900

    [CNG] 2667번: 단지번호붙이기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -4,49 +4,56 @@ import java.util.stream.Collectors;
 class P {
 	int row;
 	int col;
-	int time;
-	public P(int row, int col, int time) {
+	public P(int row, int col) {
 		this.row = row;
 		this.col = col;
-		this.time = time;
 	}
 }
 public class Main {
-	private static int N, M;
+	private static int N;
 	private static int[][] board;
 	private static Queue<P> queue;
 	private static int[] dcol = {0,0,-1,1};
 	private static int[] drow = {-1,1,0,0};
+	private static ArrayList<Integer> result;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		board = new int[N][M];
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
 		for(int i = 0; i < N; i++) {
 			String line = br.readLine();
-			for(int j = 0; j < M; j++) {
+			for(int j = 0; j < N; j++) {
 				board[i][j] = line.charAt(j) - '0';
 			}
 		}
 		queue = new LinkedList<>();
-		queue.add(new P(0,0,1));
-		int minTime = Integer.MAX_VALUE;
-		while(!queue.isEmpty()) {
-			P p = queue.poll();
-			if(board[p.row][p.col] != 1) continue;
-			if(p.row == N-1 && p.col == M-1) {
-				minTime = Math.min(minTime, p.time);
-			}
-			board[p.row][p.col] = 2;
-			for(int i=0;i<4;i++) {
-				int nrow = p.row + drow[i];
-				int ncol = p.col + dcol[i];
-				if(nrow < 0 || nrow >= N || ncol < 0 || ncol >= M) continue;
-				if(board[nrow][ncol] != 1) continue;
-				queue.add(new P(nrow, ncol, p.time+1));
+		result = new ArrayList<>();
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(board[i][j] == 1) {
+					int cnt = 0;
+					queue.add(new P(i,j));
+					while(!queue.isEmpty()) {
+						P p = queue.poll();
+						if(board[p.row][p.col] != 1) continue;
+						board[p.row][p.col] = 2;
+						cnt++;
+						for(int t=0;t<4;t++) {
+							int nrow = p.row + drow[t];
+							int ncol = p.col + dcol[t];
+							if(nrow < 0 || nrow >= N || ncol < 0 || ncol >= N) continue;
+							if(board[nrow][ncol] != 1) continue;
+							queue.add(new P(nrow, ncol));
+						}
+					}
+					result.add(cnt);
+				}
 			}
 		}
-		System.out.println(minTime);
+		Collections.sort(result);
+		System.out.println(result.size());
+		for(int i=0;i<result.size();i++) {
+			System.out.println(result.get(i));
+		}
 	}
 }
