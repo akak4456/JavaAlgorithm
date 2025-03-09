af22a9cffc78c66787e4bbad4e41f1e0ca099507
akak4456 <akak4456@naver.com>
2025-03-10 08:35:28 +0900

    [CNG] 11403번: 경로 찾기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -18,22 +18,60 @@ class P implements Comparable<P> {
 }
 public class Main {
 	private static int N;
-	private static PriorityQueue<P> pq;
+	private static final int INF = 987654321;
+	private static int[][] dist;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		pq = new PriorityQueue<>();
-		for (int i = 0; i < N; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if(x == 0) {
-				if(pq.isEmpty()) {
-					System.out.println(0);
+		dist = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < N; j++) {
+				int a = Integer.parseInt(st.nextToken());
+				if(a == 0) {
+					if(i == j) {
+						dist[i][j] = 0;
+					} else {
+						dist[i][j] = INF;
+					}
 				} else {
-					System.out.println(pq.poll().value);
+					dist[i][j] = 1;
 				}
-			} else {
-				pq.add(new P(x));
 			}
+		}
+		for(int k=0;k<N;k++) {
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<N;j++) {
+					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(i == j) {
+					boolean isPossible = false;
+					for(int k=0;k<N;k++) {
+						if(i == k) continue;
+						if(dist[i][k] != INF && dist[k][i] != INF) {
+							isPossible = true;
+							break;
+						}
+					}
+					if(isPossible) {
+						System.out.print("1 ");
+					} else {
+						System.out.print("0 ");
+					}
+				} else {
+					if(dist[i][j] == INF) {
+						System.out.print("0 ");
+					} else {
+						System.out.print("1 ");
+					}
+				}
+			}
+			System.out.println();
 		}
 	}
 }
