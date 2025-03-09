ee244028b019e6c961fbb1c4cc0e104a7a1e1f7a
akak4456 <akak4456@naver.com>
2025-03-09 09:28:59 +0900

    [CNG] 5525번: IOIOI 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,31 @@
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
-class P {
-	int row;
-	int col;
-	public P(int row, int col) {
-		this.row = row;
-		this.col = col;
-	}
-}
 public class Main {
-	private static int N;
-	private static int[][] board;
-	private static Queue<P> queue;
-	private static int[] dcol = {0,0,-1,1};
-	private static int[] drow = {-1,1,0,0};
-	private static ArrayList<Integer> result;
+	private static int N, M;
+	private static String S;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		for(int i = 0; i < N; i++) {
-			String line = br.readLine();
-			for(int j = 0; j < N; j++) {
-				board[i][j] = line.charAt(j) - '0';
-			}
-		}
-		queue = new LinkedList<>();
-		result = new ArrayList<>();
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(board[i][j] == 1) {
-					int cnt = 0;
-					queue.add(new P(i,j));
-					while(!queue.isEmpty()) {
-						P p = queue.poll();
-						if(board[p.row][p.col] != 1) continue;
-						board[p.row][p.col] = 2;
-						cnt++;
-						for(int t=0;t<4;t++) {
-							int nrow = p.row + drow[t];
-							int ncol = p.col + dcol[t];
-							if(nrow < 0 || nrow >= N || ncol < 0 || ncol >= N) continue;
-							if(board[nrow][ncol] != 1) continue;
-							queue.add(new P(nrow, ncol));
-						}
-					}
-					result.add(cnt);
+		M = Integer.parseInt(br.readLine());
+		S = br.readLine();
+		int startPos = 0;
+		int result = 0;
+		int count = 0;
+		while(startPos < M) {
+			// System.out.println(startPos + " " + endPos);
+			if(startPos + 3 <= M && S.substring(startPos, startPos + 3).equals("IOI")) {
+				count++;
+				startPos += 2;
+				if(count == N) {
+					result++;
+					count--;
 				}
+			} else {
+				count = 0;
+				startPos++;
 			}
 		}
-		Collections.sort(result);
-		System.out.println(result.size());
-		for(int i=0;i<result.size();i++) {
-			System.out.println(result.get(i));
-		}
+		System.out.println(result);
 	}
 }
