30e143752d7d68402d80496f46d1da0a2e455e57
akak4456 <akak4456@naver.com>
2025-03-11 08:17:10 +0900

    [CNG] 1931번: 회의실 배정 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,44 @@
 import java.io.*;
 import java.util.*;
-class P {
-	int row;
-	int col;
-	int time;
+class P implements Comparable<P> {
+	int start;
+	int end;
 
-	public P(int row, int col, int time) {
-		this.row = row;
-		this.col = col;
-		this.time = time;
+	public P(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+
+	@Override
+	public int compareTo(P o) {
+		if(this.end == o.end) {
+			return Integer.compare(this.start, o.start);
+		}
+		return Integer.compare(this.end, o.end);
 	}
 }
 public class Main {
-	private static int N, r, c;
-	private static int solve(int startRow, int startCol, int endRow, int endCol) {
-		if(endRow - startRow == 1 && endCol - startCol == 1) {
-			int rowDist = r - startRow;
-			int colDist = c - startCol;
-			if(rowDist == 0) {
-				if(colDist == 0) {
-					return 0;
-				} else {
-					return 1;
-				}
-			} else {
-				if(colDist == 0) {
-					return 2;
-				} else {
-					return 3;
-				}
-			}
-		}
-		int midRow = (startRow + endRow) / 2;
-		int midCol = (startCol + endCol) / 2;
-		int cnt = ((endRow - startRow + 1) / 2) * ((endCol - startCol + 1) / 2);
-		if(startRow <= r && r <= midRow && startCol <= c && c <= midCol) {
-			return solve(startRow, startCol, midRow, midCol);
-		} else if(startRow <= r && r <= midRow && midCol + 1 <= c && c <= endCol) {
-			return solve(startRow, midCol + 1, midRow, endCol) + cnt;
-		} else if(midRow + 1 <= r && r <= endRow && startCol <= c && c <= midCol) {
-			return solve(midRow + 1, startCol, endRow, midCol) + cnt * 2;
-		} else {
-			return solve(midRow + 1, midCol + 1, endRow, endCol) + cnt * 3;
-		}
-	}
+	private static int N;
+	private static P[] pair;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		int endVal = (int)Math.pow(2, N);
-		System.out.println(solve(0,0,endVal-1, endVal-1));
+		N = Integer.parseInt(br.readLine());
+		pair = new P[N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			pair[i] = new P(a, b);
+		}
+		Arrays.sort(pair);
+		int endTime = 0;
+		int cnt = 0;
+		for(int i=0;i<N;i++) {
+			if(pair[i].start >= endTime) {
+				endTime = pair[i].end;
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
 	}
 }
