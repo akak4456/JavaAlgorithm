03ba2cd98d62bacd9ca75b1792fcc56386bebc45
akak4456 <akak4456@naver.com>
2025-03-17 10:41:00 +0900

    [CNG] 7662번: 이중 우선순위 큐 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,40 @@
 import java.io.*;
 import java.util.*;
-class P{
-	int pos;
-	int time;
-	public P(int pos, int time){
-		this.pos = pos;
-		this.time = time;
-	}
-}
 public class Main {
-	private static int N, M;
-	private static int[] ladder;
-	private static int[] snake;
-	private static Queue<P> queue;
-	private static boolean[] visited;
+	private static int T;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		ladder = new int[100 + 1];
-		snake = new int[100 + 1];
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			ladder[a] = b;
-		}
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			snake[a] = b;
-		}
-		queue = new LinkedList<>();
-		queue.add(new P(1, 0));
-		visited = new boolean[100 + 1];
-		int minTime = Integer.MAX_VALUE;
-		while(!queue.isEmpty()) {
-			P p = queue.poll();
-			if(visited[p.pos]) continue;
-			visited[p.pos] = true;
-			if(p.pos == 100) {
-				minTime = Math.min(minTime, p.time);
-				continue;
+		T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int testCase = 0; testCase < T;testCase++) {
+			int k = Integer.parseInt(br.readLine());
+			TreeMap<Integer, Integer> map = new TreeMap<>();
+			for(int op=0;op<k;op++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				String opCode = st.nextToken();
+				if("I".equals(opCode)) {
+					int operand = Integer.parseInt(st.nextToken());
+					map.put(operand, map.getOrDefault(operand, 0) + 1);
+				} else {
+					if(map.size() == 0) continue;
+					int operand = Integer.parseInt(st.nextToken());
+					int num;
+					if(operand == 1) {
+						num = map.lastKey();
+					} else {
+						num = map.firstKey();
+					}
+					if(map.put(num, map.get(num) - 1) == 1) {
+						map.remove(num);
+					}
+				}
 			}
-			for(int i=1;i<=6;i++) {
-				int newPos = p.pos + i;
-				if(newPos > 100) break;
-				if(ladder[newPos] != 0) {
-					newPos = ladder[newPos];
-				}
-				if(snake[newPos] != 0) {
-					newPos = snake[newPos];
-				}
-				queue.add(new P(newPos, p.time+1));
+			if(map.size() == 0) {
+				sb.append("EMPTY\n");
+			} else {
+				sb.append(map.lastKey() + " " + map.firstKey() + "\n");
 			}
 		}
-		System.out.println(minTime);
+		System.out.println(sb);
 	}
 }
