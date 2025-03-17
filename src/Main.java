50d2c9f2bbb0c335f6657d934cffed3d6a09f34a
akak4456 <akak4456@naver.com>
2025-03-17 10:48:27 +0900

    [CNG] 14500번:ㅣ 테트로미노 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,83 @@
+// https://hanyeop.tistory.com/416 참고함
+// 테스트 위한 목적임
+// [백준] 14500. 테트로미노 (Java)
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	static int max = Integer.MIN_VALUE;
+	static int[][] arr;
+	static boolean[][] visit;
+	static int n;
+	static int m;
+
+	// 상하좌우
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int t = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		while (t-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n][m];
+		visit = new boolean[n][m];
 
-			Queue<Integer> q = new LinkedList<>();
-			boolean[] visited = new boolean[10000]; // BFS 탐색의 방문 여부 체크
-			String[] command = new String[10000]; // 정답 명령어를 담는 배열
-
-
-			q.add(a); // a를 큐에 담고
-			visited[a] = true; // 방문 표시한다
-			Arrays.fill(command,  ""); // 꼭 넣어야하나? 안넣으면 틀림
-
-
-			while (!q.isEmpty() && !visited[b]) {
-				int now = q.poll(); // 큐에서 값을 뺀다 = 현재 탐색 위치
-
-				int D = (2 * now) % 10000; // n을 두배로 바꾸고 10000으로 나눈 나머지
-				int S = now == 0 ? 9999 : now - 1; // 0일 때, 9999, 아니면 1을 빼준다
-				int L = (now % 1000) * 10 + now / 1000; // 1234 -> 2341 : 1234를 1000으로 나눈 나머지(234)에 10을 곱함=2340, 1234를 1000으로 나누면 1, 2340+1=2341
-				int R = (now % 10) * 1000 + now / 10; // 1234 -> 4123 : 1234를 10으로 나눈 나머지에 1000 곱합 = 4000, 1234를 10으로 나누면 123, 4000+123=4123
-
-				if (!visited[D]) {
-					q.add(D); // 큐에 넣는다
-					visited[D] = true; // 방문처리한다
-					command[D] = command[now] + "D"; // 명령어를 추가한다
-				}
-
-				if (!visited[S]) {
-					q.add(S);
-					visited[S] = true;
-					command[S] = command[now] + "S";
-				}
-
-				if (!visited[L]) {
-					q.add(L);
-					visited[L] = true;
-					command[L] = command[now] + "L";
-				}
-
-				if (!visited[R]) {
-					q.add(R);
-					visited[R] = true;
-					command[R] = command[now] + "R";
-				}
+		// 입력
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
-			System.out.println(command[b]);
+		}
+
+		// 전체 탐색 (dfs)
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				visit[i][j] = true;
+				solve(i,j,arr[i][j],1);
+				visit[i][j] = false;
+			}
+		}
+
+		System.out.println(max);
+	}
+
+	static void solve(int row, int col, int sum, int count) {
+
+		// 테트로미노 완성 시 수들의 합 계산
+		if(count == 4) {
+			max = Math.max(max, sum);
+			return;
+		}
+
+		// 상하좌우 탐색
+		for(int i = 0; i < 4; i++) {
+			int curRow = row + dx[i];
+			int curCol = col + dy[i];
+
+			// 범위 벗어나면 예외 처리
+			if(curRow < 0 || curRow >= n || curCol < 0 || curCol >= m) {
+				continue;
+			}
+
+			// 아직 방문하지 않은 곳이라면
+			if(!visit[curRow][curCol]) {
+
+				// 보라색(ㅗ) 테트로미노 만들기 위해 2번째 칸에서 탐색 한번 더 진행
+				if(count == 2) {
+					visit[curRow][curCol] = true;
+					solve(row, col, sum + arr[curRow][curCol], count + 1);
+					visit[curRow][curCol] = false;
+				}
+
+				visit[curRow][curCol] = true;
+				solve(curRow, curCol, sum + arr[curRow][curCol], count + 1);
+				visit[curRow][curCol] = false;
+			}
 		}
 	}
 }
