0337dc7472dea47d45f14ac2f4b1bd0b92f0a47c
akak4456 <akak4456@naver.com>
2025-03-24 11:06:03 +0900

    [CNG] 11053번: 가장 긴 증가하는 부분 수열 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	private static int N, M;
-
-	private static int[] arr;
-
-	private static StringBuilder sb = new StringBuilder();
-
-	private static void solve(int used, ArrayList<Integer> list) {
-		if(list.size() == M) {
-			for(int i=0;i<list.size();i++) {
-				sb.append(list.get(i)).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		for(int i=0;i<N;i++) {
-			if((used & (1 << i)) > 0) continue;
-			list.add(arr[i]);
-			solve(used | (1 << i), list);
-			list.remove(list.size() - 1);
-		}
-	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		solve(0,new ArrayList<>());
-		System.out.println(sb);
-	}
+    private static int N;
+    private static int[] arr;
+    private static int[][] dp;
+	private static int solve(int startIdx, int curMinValue) {
+        if(startIdx == N) return 0;
+        int ret = dp[startIdx][curMinValue];
+        if(ret != -1) return ret;
+        ret = solve(startIdx + 1, curMinValue);
+        if(arr[startIdx] > curMinValue) {
+            ret = Math.max(ret, solve(startIdx + 1, arr[startIdx]) + 1);
+        }
+        dp[startIdx][curMinValue] = ret;
+        return ret;
+    }
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp = new int[N][1000 + 1];
+        for(int i=0;i<N;i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println(solve(0, 0));
+    }
 
 }
