e5ce262f5abdda77ce288117fd46b3085eb9b26e
akak4456 <akak4456@naver.com>
2025-03-25 11:06:01 +0900

    [CNG] 11725번: 트리의 부모 찾기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,40 @@ import java.util.*;
 
 public class Main {
     private static int N;
-    private static int[] arr;
-    private static int[][] dp;
-	private static int solve(int startIdx, int curMinValue) {
-        if(startIdx == N) return 0;
-        int ret = dp[startIdx][curMinValue];
-        if(ret != -1) return ret;
-        ret = solve(startIdx + 1, curMinValue);
-        if(arr[startIdx] > curMinValue) {
-            ret = Math.max(ret, solve(startIdx + 1, arr[startIdx]) + 1);
+    private static ArrayList<ArrayList<Integer>> adj;
+    private static boolean[] visited;
+    private static int[] parent;
+    private static void dfs(int par, int node) {
+        if(visited[node]) return;
+        visited[node] = true;
+        parent[node] = par;
+        for(int i=0;i<adj.get(node).size();i++) {
+            int adjNode = adj.get(node).get(i);
+            if(!visited[adjNode]) {
+                dfs(node, adjNode);
+            }
         }
-        dp[startIdx][curMinValue] = ret;
-        return ret;
     }
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        adj = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
         }
-        dp = new int[N][1000 + 1];
-        for(int i=0;i<N;i++) {
-            Arrays.fill(dp[i], -1);
+        visited = new boolean[N + 1];
+        parent = new int[N + 1];
+        for(int i=0;i<N-1;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adj.get(a).add(b);
+            adj.get(b).add(a);
         }
-        System.out.println(solve(0, 0));
+        dfs(0, 1);
+        for(int i=2;i<=N;i++) {
+            System.out.println(parent[i]);
+        }
     }
 
 }
