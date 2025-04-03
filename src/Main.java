fc6944517b837efee2b6530bbac44a366b567346
akak4456 <akak4456@naver.com>
2025-04-03 14:09:57 +0900

    [CNG] 16953번: A -> B 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-class N implements Comparable<N> {
-    int arr[];
-    public N(ArrayList<Integer> list) {
-        this.arr = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            this.arr[i] = list.get(i);
-        }
-    }
-
-    @Override
-    public int compareTo(N o) {
-        for(int i=0;i<this.arr.length;i++) {
-            if(this.arr[i] != o.arr[i]) return Integer.compare(this.arr[i], o.arr[i]);
-        }
-        return 0;
-    }
-}
-
 public class Main {
-    private static int N, M;
-    private static int[] arr;
-    private static TreeSet<N> set;
-    private static void solve(ArrayList<Integer> list, int start) {
-        if(list.size() == M) {
-            set.add(new N(list));
-            return;
-        }
-        for(int i=start;i<N;i++) {
-            list.add(arr[i]);
-            solve(list, i);
-            list.remove(list.size() - 1);
-        }
-    }
+    private static int A, B;
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-        set = new TreeSet<>();
-        solve(new ArrayList<>(), 0);
-        StringBuilder sb = new StringBuilder();
-        for (N s : set) {
-            for(int i=0;i<M;i++) {
-              sb.append(s.arr[i]).append(" ");
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        boolean isPossible = true;
+        int cnt = 0;
+        while(B > A) {
+            if(B % 2 == 0) {
+                cnt++;
+                B /= 2;
+            } else if(B % 10 == 1) {
+                cnt++;
+                B /= 10;
+            } else {
+                isPossible = false;
+                break;
             }
-            sb.append("\n");
         }
-        System.out.println(sb);
+        if(isPossible && A == B) {
+            System.out.println(cnt + 1);
+        } else {
+            System.out.println(-1);
+        }
     }
 
 }
