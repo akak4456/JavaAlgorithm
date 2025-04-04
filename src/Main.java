a2a2963be6c470614b5916daaeb3bf70b6557b7e
akak4456 <akak4456@naver.com>
2025-04-05 07:15:23 +0900

    [CNG] 1149번: RGB거리 풀었음

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int A, B;
+    private static int N;
+    private static int[][] arr;
+    private static int[][] dp;
+    private static int solve(int homeNumber, int colorNumber) {
+        if(homeNumber == N) {
+            return 0;
+        }
+        int ret = dp[homeNumber][colorNumber];
+        if(ret != -1) return ret;
+        ret = 987654321;
+        for(int i=0;i<3;i++) {
+            if(i == colorNumber) continue;
+            ret = Math.min(ret, solve(homeNumber + 1, i) + arr[homeNumber][colorNumber]);
+        }
+        dp[homeNumber][colorNumber] = ret;
+        return ret;
+    }
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        boolean isPossible = true;
-        int cnt = 0;
-        while(B > A) {
-            if(B % 2 == 0) {
-                cnt++;
-                B /= 2;
-            } else if(B % 10 == 1) {
-                cnt++;
-                B /= 10;
-            } else {
-                isPossible = false;
-                break;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][3];
+        dp = new int[N][3];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        if(isPossible && A == B) {
-            System.out.println(cnt + 1);
-        } else {
-            System.out.println(-1);
+        for(int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], -1);
         }
+        System.out.println(Math.min(Math.min(solve(0,0), solve(0,1)), solve(0,2)));
     }
 
 }
