8256de6f5e1a0a85a7f22991906a7e9627d8b72b
akak4456 <akak4456@naver.com>
2025-04-08 14:45:03 +0900

    [CNG] 12865번: 평범한 배낭 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,39 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    private static String str1;
-    private static String str2;
-    private static int[][] dp; // dp[i][j] = str1 의 (i..) str2 의 (j..) 의 LCS
-    public static int solve(int str1Idx, int str2Idx) {
-        if(str1Idx == str1.length() || str2Idx == str2.length()) {
-            return 0;
-        }
-        int ret = dp[str1Idx][str2Idx];
+    private static int N, K;
+    private static int[] W;
+    private static int[] V;
+    private static int[][] dp;
+    private static int solve(int pos, int remainWeight) {
+        if(pos >= N) return 0;
+        if(remainWeight <= 0) return 0;
+        int ret = dp[pos][remainWeight];
         if(ret != -1) return ret;
-        ret = solve(str1Idx + 1, str2Idx);
-        ret = Math.max(ret, solve(str1Idx, str2Idx + 1));
-        if(str1.charAt(str1Idx) == str2.charAt(str2Idx)) {
-            ret = Math.max(ret, solve(str1Idx + 1, str2Idx + 1) + 1);
+        ret = solve(pos + 1, remainWeight);
+        if(remainWeight >= W[pos]) {
+            ret = Math.max(ret, solve(pos + 1, remainWeight - W[pos]) + V[pos]);
         }
-        dp[str1Idx][str2Idx] = ret;
+        dp[pos][remainWeight] = ret;
         return ret;
     }
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        str1 = br.readLine();
-        str2 = br.readLine();
-        dp = new int[str1.length()][str2.length()];
-        for(int i = 0; i < str1.length(); i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        W = new int[N];
+        V = new int[N];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
+        dp = new int[N][K + 1];
+        for(int i = 0; i < N; i++) {
             Arrays.fill(dp[i], -1);
         }
-        System.out.println(solve(0,0));
+        System.out.println(solve(0, K));
     }
 
 }
