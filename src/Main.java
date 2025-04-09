1337f440752191094ad51cae71f0d6015ab2ff69
akak4456 <akak4456@naver.com>
2025-04-09 21:05:43 +0900

    [CNG] 13549번: 숨바꼭질 3 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,53 @@
 import java.io.*;
 import java.util.*;
-public class Main {
-    private static int N, K;
-    private static int[] W;
-    private static int[] V;
-    private static int[][] dp;
-    private static int solve(int pos, int remainWeight) {
-        if(pos >= N) return 0;
-        if(remainWeight <= 0) return 0;
-        int ret = dp[pos][remainWeight];
-        if(ret != -1) return ret;
-        ret = solve(pos + 1, remainWeight);
-        if(remainWeight >= W[pos]) {
-            ret = Math.max(ret, solve(pos + 1, remainWeight - W[pos]) + V[pos]);
-        }
-        dp[pos][remainWeight] = ret;
-        return ret;
+class PQNode implements Comparable<PQNode> {
+    int current;
+    int dist;
+
+    public PQNode(int current, int dist) {
+        this.current = current;
+        this.dist = dist;
     }
+
+    @Override
+    public int compareTo(PQNode o) {
+        return Integer.compare(dist, o.dist);
+    }
+}
+public class Main {
+    private static final int INF = 987654321;
+    private static int N, K;
+    private static int[] d;
+    private static PriorityQueue<PQNode> pq;
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        W = new int[N];
-        V = new int[N];
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
+        pq = new PriorityQueue<>();
+        d = new int[100000 + 1];
+        Arrays.fill(d, INF);
+        d[N] = 0;
+        pq.add(new PQNode(N, 0));
+        while(!pq.isEmpty()) {
+            PQNode cur = pq.poll();
+            int current = cur.current;
+            int dist = cur.dist;
+            if(d[current] < dist) continue;
+            if(current - 1 >= 0 && d[current - 1] > dist + 1) {
+                d[current - 1] = dist + 1;
+                pq.add(new PQNode(current - 1, dist + 1));
+            }
+            if(current + 1 <= 100000 && d[current + 1] > dist + 1) {
+                d[current + 1] = dist + 1;
+                pq.add(new PQNode(current + 1, dist + 1));
+            }
+            if(current * 2 <= 100000 && d[current * 2] > dist) {
+                d[current * 2] = dist;
+                pq.add(new PQNode(current * 2, dist));
+            }
         }
-        dp = new int[N][K + 1];
-        for(int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        System.out.println(solve(0, K));
+        System.out.println(d[K]);
     }
 
 }
