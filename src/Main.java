ab2f28bf4f5b2249d05614f849d78d06ce5ed7b8
akak4456 <akak4456@naver.com>
2025-04-11 11:19:34 +0900

    [CNG] 17070번: 파이프 옮기기 1 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,62 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    private static int N, M;
+    private static int N;
     private static int[][] board;
-    private static int result = 987654321;
-    private static void solve(int idx) {
-        int newIdx = idx;
-        while(newIdx < N * N) {
-            int row = newIdx / N;
-            int col = newIdx % N;
-            if(board[row][col] == 2) {
-                board[row][col] = 0;
-                solve(newIdx + 1);
-                board[row][col] = 2;
-            }
-            newIdx++;
+    private static int[][][] dp;
+    private static int solve(int row, int col, int type) {
+        // System.out.println(row + " " + col + " " + type);
+        if(row == N - 1 && col == N - 1) {
+            return 1;
         }
-        int chickenCount = 0;
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                if(board[i][j] == 2) {
-                    chickenCount++;
-                }
-            }
+        int ret = dp[row][col][type];
+        if(ret != -1) return ret;
+        ret = 0;
+        //type == 0: 가로 type == 1: 세로 type == 2: 대각선
+        if((type == 0 || type == 2) && col + 1 < N && board[row][col + 1] == 0) {
+            board[row][col + 1] = 1;
+            ret += solve(row, col + 1, 0);
+            board[row][col + 1] = 0;
         }
-        if(chickenCount == 0 || chickenCount > M) return;
-        int res = 0;
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                if(board[i][j] == 1) {
-                    int minDist = Integer.MAX_VALUE;
-                    for(int k=0;k<N;k++) {
-                        for(int l=0;l<N;l++) {
-                            if(board[k][l] == 2) {
-                                minDist = Math.min(minDist, Math.abs(i - k) + Math.abs(j - l));
-                            }
-                        }
-                    }
-                    res += minDist;
-                }
-            }
+        if((type == 1 || type == 2) && row + 1 < N && board[row + 1][col] == 0) {
+            board[row + 1][col] = 1;
+            ret += solve(row + 1, col, 1);
+            board[row + 1][col] = 0;
         }
-        result = Math.min(result, res);
+        if((type == 0 || type == 1 || type == 2) && row + 1 < N && col + 1 < N && board[row][col + 1] == 0 && board[row + 1][col] == 0 && board[row + 1][col + 1] == 0) {
+            board[row][col + 1] = 1;
+            board[row + 1][col] = 1;
+            board[row + 1][col + 1] = 1;
+            ret += solve(row + 1, col + 1, 2);
+            board[row][col + 1] = 0;
+            board[row + 1][col] = 0;
+            board[row + 1][col + 1] = 0;
+        }
+        dp[row][col][type] = ret;
+        return ret;
     }
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
         board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
+        dp = new int[N][N][3];
+        for(int i=0;i<N;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0;j<N;j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        solve(0);
-        System.out.println(result);
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                for(int k=0;k<3;k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        board[0][0] = 1;
+        board[0][1] = 1;
+        solve(0,1,0);
+        System.out.println(dp[0][1][0]);
     }
 
 }
