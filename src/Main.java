243ac2ad0d920a3cc16550a1a6694dc128b969ec
akak4456 <akak4456@naver.com>
2025-04-13 14:19:15 +0900

    [CNG] 1043번: 거짓말 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,94 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    private static int N;
-    private static int[][] board;
-    private static int[][][] dp;
-    private static int solve(int row, int col, int type) {
-        // System.out.println(row + " " + col + " " + type);
-        if(row == N - 1 && col == N - 1) {
-            return 1;
+    private static int N, M;
+    private static int truthCnt;
+    private static int[] truth;
+    private static ArrayList<ArrayList<Integer>> party;
+    private static int[] root;
+    private static int[] rank;
+    private static int find(int x) {
+        if(root[x] == x){
+            return x;
+        } else {
+            root[x] = find(root[x]);
+            return root[x];
         }
-        int ret = dp[row][col][type];
-        if(ret != -1) return ret;
-        ret = 0;
-        //type == 0: 가로 type == 1: 세로 type == 2: 대각선
-        if((type == 0 || type == 2) && col + 1 < N && board[row][col + 1] == 0) {
-            board[row][col + 1] = 1;
-            ret += solve(row, col + 1, 0);
-            board[row][col + 1] = 0;
+    }
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x == y) {
+            return;
         }
-        if((type == 1 || type == 2) && row + 1 < N && board[row + 1][col] == 0) {
-            board[row + 1][col] = 1;
-            ret += solve(row + 1, col, 1);
-            board[row + 1][col] = 0;
+        if(rank[x] < rank[y]) {
+            root[x] = y;
+        } else {
+            root[y] = x;
+            if(rank[x] == rank[y]) {
+                rank[x]++;
+            }
         }
-        if((type == 0 || type == 1 || type == 2) && row + 1 < N && col + 1 < N && board[row][col + 1] == 0 && board[row + 1][col] == 0 && board[row + 1][col + 1] == 0) {
-            board[row][col + 1] = 1;
-            board[row + 1][col] = 1;
-            board[row + 1][col + 1] = 1;
-            ret += solve(row + 1, col + 1, 2);
-            board[row][col + 1] = 0;
-            board[row + 1][col] = 0;
-            board[row + 1][col + 1] = 0;
-        }
-        dp[row][col][type] = ret;
-        return ret;
     }
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        board = new int[N][N];
-        dp = new int[N][N][3];
-        for(int i=0;i<N;i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0;j<N;j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        truthCnt = Integer.parseInt(st.nextToken());
+        if(truthCnt > 0) {
+            truth = new int[truthCnt];
+            for(int i=0; i<truthCnt; i++) {
+                truth[i] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                for(int k=0;k<3;k++) {
-                    dp[i][j][k] = -1;
+        party = new ArrayList<>();
+        for(int i=0; i<M; i++) {
+            party.add(new ArrayList<>());
+            st = new StringTokenizer(br.readLine(), " ");
+            int cnt = Integer.parseInt(st.nextToken());
+            for(int j=0; j<cnt; j++) {
+                int a = Integer.parseInt(st.nextToken());
+                party.get(i).add(a);
+            }
+        }
+        root = new int[N + 1];
+        rank = new int[N + 1];
+        for(int i=0;i<=N;i++) {
+            root[i] = i;
+            rank[i] = 0;
+        }
+        for(int partyNum = 0; partyNum < M; partyNum++) {
+            ArrayList<Integer> p = party.get(partyNum);
+            for(int i=0; i<p.size(); i++) {
+                for(int j=0; j<p.size(); j++) {
+                    if(i == j) continue;
+                    union(p.get(i), p.get(j));
                 }
             }
         }
-        board[0][0] = 1;
-        board[0][1] = 1;
-        solve(0,1,0);
-        System.out.println(dp[0][1][0]);
+        int result = 0;
+        for(int partyNum = 0; partyNum < M; partyNum++) {
+            ArrayList<Integer> p = party.get(partyNum);
+            boolean isPossible = true;
+            for(int i=0; i<p.size(); i++) {
+                for(int j=0; j<truthCnt; j++) {
+                    if(find(p.get(i)) == find(truth[j])) {
+                        isPossible = false;
+                        break;
+                    }
+                }
+                if(!isPossible) {
+                    break;
+                }
+            }
+            if(isPossible) {
+                result++;
+            }
+        }
+        System.out.println(result);
     }
 
 }
