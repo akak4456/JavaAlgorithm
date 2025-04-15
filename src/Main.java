01275f8ebf195b1efe662b6ae7f29008ad2428fa
akak4456 <akak4456@naver.com>
2025-04-15 17:13:12 +0900

    [CNG] 1753번: 최단경로 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,77 @@
 import java.io.*;
 import java.util.*;
+class AdjGraphNode {
+    int adj;
+    int cost;
+
+    public AdjGraphNode(int adj, int cost) {
+        this.adj = adj;
+        this.cost = cost;
+    }
+}
+class PQNode implements Comparable<PQNode> {
+    int node;
+    int dist;
+    public PQNode(int node, int dist) {
+        this.node = node;
+        this.dist = dist;
+    }
+
+    @Override
+    public int compareTo(PQNode o) {
+        return Integer.compare(this.dist, o.dist);
+    }
+}
 public class Main {
-    private static int N, E;
-    private static int[][] dist;
-    private static final int INF = 400000000;
-    private static int v1,v2;
+    private static int V, E;
+    private static int K;
+    private static final int INF = 987654321;
+    private static ArrayList<ArrayList<AdjGraphNode>> adj;
+    private static int dist[];
+    private static PriorityQueue<PQNode> pq;
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-        dist = new int[N + 1][N + 1];
-        for(int i=0;i<=N;i++) {
-            for(int j=0;j<=N;j++) {
-                if(i == j) {
-                    dist[i][j] = 0;
-                } else {
-                    dist[i][j] = INF;
-                }
-            }
+        K = Integer.parseInt(br.readLine());
+        adj = new ArrayList<>();
+        for(int i=0;i<=V;i++) {
+            adj.add(new ArrayList<>());
         }
         for(int i=0;i<E;i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            dist[a][b] = c;
-            dist[b][a] = c;
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            adj.get(u).add(new AdjGraphNode(v, w));
         }
-        for(int k=1;k<=N;k++) {
-            for(int i=1;i<=N;i++) {
-                for(int j=1;j<=N;j++) {
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+        dist = new int[V + 1];
+        Arrays.fill(dist, INF);
+        dist[K] = 0;
+        pq = new PriorityQueue<>();
+        pq.add(new PQNode(K, 0));
+        while(!pq.isEmpty()) {
+            PQNode cur = pq.poll();
+            if(dist[cur.node] < cur.dist) continue;
+            for(int i=0;i<adj.get(cur.node).size();i++) {
+                AdjGraphNode adjNode = adj.get(cur.node).get(i);
+                int newCost = cur.dist + adjNode.cost;
+                if(newCost < dist[adjNode.adj]) {
+                    dist[adjNode.adj] = newCost;
+                    pq.add(new PQNode(adjNode.adj, newCost));
                 }
             }
         }
-        st = new StringTokenizer(br.readLine(), " ");
-        v1 = Integer.parseInt(st.nextToken());
-        v2 = Integer.parseInt(st.nextToken());
-        int d1 = dist[1][v1] + dist[v1][v2] + dist[v2][N];
-        int d2 = dist[1][v2] + dist[v2][v1] + dist[v1][N];
-        if(d1 >= INF && d2 >= INF) {
-            System.out.println(-1);
-        } else {
-            System.out.println(Math.min(d1, d2));
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<=V;i++) {
+            if(dist[i] != INF) {
+                sb.append(dist[i]).append("\n");
+            } else {
+                sb.append("INF\n");
+            }
         }
+        System.out.println(sb);
     }
 
 }
