b15eb9394fdf926a431a4776b82257e7af5db636
akak4456 <akak4456@naver.com>
2025-05-01 16:20:42 +0900

    [CNG] 14502번: 연구소 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,91 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+class GraphPair {
+    int row;
+    int col;
+    public GraphPair(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
 public class Main {
-    private static int M;
-    private static long[] N;
-    private static long[] S;
-    private static final long MOD = 1_000_000_007;
-    private static Map<Long, Long> cache = new HashMap<>();
-    private static long pow(long base, long exp) {
-        if(exp == 1) {
-            return base;
+    private static int N, M;
+    private static int[][] board;
+    private static boolean[][] visited;
+    private static int result = 0;
+    private static int[] drow = {-1,1,0,0};
+    private static int[] dcol = {0,0,-1,1};
+    private static void spread(int[][] spreadBoard) {
+        Queue<GraphPair> pairs = new LinkedList<GraphPair>();
+        for(int row = 0; row < N; row++) {
+            for(int col = 0; col < M; col++) {
+                if(spreadBoard[row][col] == 2) {
+                    pairs.add(new GraphPair(row, col));
+                }
+            }
         }
-        if(cache.containsKey(exp)) {
-            return cache.get(exp);
+        while(!pairs.isEmpty()) {
+            GraphPair pair = pairs.poll();
+            if(spreadBoard[pair.row][pair.col] == 1) continue;
+            spreadBoard[pair.row][pair.col] = 1;
+            for(int i=0;i<4;i++) {
+                int nrow = pair.row + drow[i];
+                int ncol = pair.col + dcol[i];
+                if(nrow < 0 || nrow >= N || ncol < 0 || ncol >= M) continue;
+                if(spreadBoard[nrow][ncol] != 0) continue;
+                pairs.add(new GraphPair(nrow, ncol));
+            }
         }
-        if(exp % 2 == 0) {
-            long ret = (pow(base, exp / 2) * pow(base, exp / 2)) % MOD;
-            cache.put(exp, ret);
-            return ret;
-        } else {
-            long ret = (base * pow(base, exp - 1)) % MOD;
-            cache.put(exp, ret);
-            return ret;
+        int cnt = 0;
+        for(int row = 0; row < N; row++) {
+            for(int col = 0; col < M; col++) {
+                if(spreadBoard[row][col] == 0) {
+                    cnt++;
+                }
+            }
+        }
+        result = Math.max(result, cnt);
+    }
+    private static void makeWallAndSpread(int curCnt) {
+        if(curCnt == 3) {
+            int[][] spreadBoard = new int[N][M];
+            for(int row = 0; row < N; row++) {
+                for(int col = 0; col < M; col++) {
+                    spreadBoard[row][col] = board[row][col];
+                }
+            }
+            spread(spreadBoard);
+            return;
+        }
+        for(int row = 0; row < N; row++) {
+            for(int col = 0; col < M; col++) {
+                if(board[row][col] == 0) {
+                    board[row][col] = 1;
+                    makeWallAndSpread(curCnt + 1);
+                    board[row][col] = 0;
+                }
+            }
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        M = Integer.parseInt(br.readLine());
-        N = new long[M];
-        S = new long[M];
-        long sum = 0;
-        for (int i=0;i<M;i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            N[i] = Long.parseLong(st.nextToken());
-            S[i] = Long.parseLong(st.nextToken());
-            cache.clear();
-            sum += (S[i] * pow(N[i], MOD - 2));
-            sum %= MOD;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-        System.out.println(sum);
+        makeWallAndSpread(0);
+        System.out.println(result);
     }
 }
