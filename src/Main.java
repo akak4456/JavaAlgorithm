b7f7d99bb2257a62286b838f2dc775260dab5606
akak4456 <akak4456@naver.com>
2025-05-02 19:32:27 +0900

    [CNG] 사전 순 최대 공통 부분 수열 풀었음

--- src/Main.java
+++ src/Main.java
@@ -1,101 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int R, C, T;
-    private static int[][] board;
-    private static int[] drow = {-1,1,0,0};
-    private static int[] dcol = {0,0,-1,1};
-    private static int upperRow = -1;
-    private static int lowerRow = -1;
+    private static int N;
+    private static int[] A;
+    private static int M;
+    private static int[] B;
+    private static ArrayList<Integer> result;
+    private static void solve(int aStartIdx, int bStartIdx) {
+        if(aStartIdx >= N || bStartIdx >= M) return;
+        int maxVal = 0;
+        int aNext = 0;
+        int bNext = 0;
+        for(int aIdx = aStartIdx; aIdx < N; aIdx++) {
+            for(int bIdx = bStartIdx; bIdx < M; bIdx++) {
+                if(A[aIdx] == B[bIdx] && maxVal < A[aIdx]) {
+                    maxVal = A[aIdx];
+                    aNext = aIdx;
+                    bNext = bIdx;
+                    break;
+                }
+            }
+        }
+        if(maxVal != 0) {
+            result.add(maxVal);
+            solve(aNext + 1, bNext + 1);
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        T = Integer.parseInt(st.nextToken());
-        board = new int[R][C];
-        for(int i = 0; i < R; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < C; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if(board[i][j] == -1) {
-                    if(upperRow == -1) {
-                        upperRow = i;
-                    } else {
-                        lowerRow = i;
-                    }
-                }
-            }
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        for(int time = 0; time < T; time++) {
-            // 미세먼지가 확산된다.
-            int[][] dBoard = new int[R][C];
-            for(int i=0;i<R;i++) {
-                for(int j=0;j<C;j++) {
-                    if(board[i][j] > 0) {
-                        int amount = board[i][j] / 5;
-                        int spreadDirectionCnt = 0;
-                        for(int t=0;t<4;t++) {
-                            int nrow = i + drow[t];
-                            int ncol = j + dcol[t];
-                            if(nrow < 0 || nrow >= R || ncol < 0 || ncol >= C) continue;
-                            if(board[nrow][ncol] == -1) continue;
-                            dBoard[nrow][ncol] += amount;
-                            spreadDirectionCnt++;
-                        }
-                        dBoard[i][j] -= amount * spreadDirectionCnt;
-                    }
-                }
-            }
-            for(int i=0;i<R;i++) {
-                for(int j=0;j<C;j++) {
-                    if(board[i][j] == -1) continue;
-                    board[i][j] += dBoard[i][j];
-                }
-            }
-            // 공기청정기가 작동한다.
-            // 윗부분 작동
-            for(int row = upperRow - 2; row >= 0; row--) {
-                board[row + 1][0] = board[row][0];
-            }
-            for(int col = 1; col < C; col++) {
-                board[0][col - 1] = board[0][col];
-            }
-            for(int row = 1; row <= upperRow; row++) {
-                board[row - 1][C - 1] = board[row][C - 1];
-            }
-            for(int col = C - 2; col >= 1; col--) {
-                board[upperRow][col + 1] = board[upperRow][col];
-            }
-            board[upperRow][1] = 0;
-            // 아랫부분 작동
-            for(int row = lowerRow + 2; row < R; row++) {
-                board[row - 1][0] = board[row][0];
-            }
-            for(int col = 1; col < C; col++) {
-                board[R - 1][col - 1] = board[R - 1][col];
-            }
-            for(int row = R - 2; row >= lowerRow; row--) {
-                board[row + 1][C - 1] = board[row][C - 1];
-            }
-            for(int col = C - 2; col >= 1; col--) {
-                board[lowerRow][col + 1] = board[lowerRow][col];
-            }
-            board[lowerRow][1] = 0;
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        B = new int[M];
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
         }
-        int remain = 0;
-        for(int row = 0; row < R; row++) {
-            for(int col = 0; col < C; col++) {
-                if(board[row][col] > 0) {
-                    remain+=board[row][col];
-                }
-            }
+        result = new ArrayList<>();
+        solve(0,0);
+        System.out.println(result.size());
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i) + " ");
         }
-        System.out.println(remain);
     }
 }
