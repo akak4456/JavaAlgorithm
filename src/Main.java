aa1b7b34efaaff2a41ab37ce5e4d4da2d2b2e3de
akak4456 <akak4456@naver.com>
2025-05-05 16:10:40 +0900

    [CNG] 1865번: 웜홀 풀었음

--- src/Main.java
+++ src/Main.java
@@ -5,44 +5,62 @@ import java.util.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int N, M, X;
-    private static int[][] dist;
+    private static int TC;
+    private static int N, M, W;
+    private static int dist[][];
     private static final int INF = 987654321;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        dist = new int[N + 1][N + 1];
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=N; j++) {
-                if(i == j) {
-                    dist[i][j] = 0;
-                } else {
-                    dist[i][j] = INF;
+        TC = Integer.parseInt(br.readLine());
+        for(int testCase = 0; testCase < TC; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            dist = new int[N + 1][N + 1];
+            for(int i=1;i<=N;i++) {
+                for(int j=1;j<=N;j++) {
+                    if(i == j) {
+                        dist[i][j] = 0;
+                    } else {
+                        dist[i][j] = INF;
+                    }
                 }
             }
-        }
-        for(int i=1; i<=M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            dist[a][b] = t;
-        }
-
-        for(int k=1;k<=N;k++) {
-            for(int i=1; i<=N; i++) {
-                for(int j=1; j<=N; j++) {
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+            for(int i=1;i<=M;i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int t = Integer.parseInt(st.nextToken());
+                dist[a][b] = Math.min(dist[a][b], t);
+                dist[b][a] = Math.min(dist[b][a], t);
+            }
+            for(int i=1;i<=W;i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int t = Integer.parseInt(st.nextToken());
+                dist[a][b] = -t;
+            }
+            for(int k=1;k<=N;k++) {
+                for(int i=1;i<=N;i++) {
+                    for(int j=1;j<=N;j++) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
                 }
             }
+            boolean isPossible = false;
+            for(int i=1;i<=N;i++) {
+                if(dist[i][i] < 0) {
+                    isPossible = true;
+                    break;
+                }
+            }
+            if(isPossible) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
         }
-        int maxTime = 0;
-        for(int i=1;i<=N;i++) {
-            maxTime = Math.max(maxTime, dist[i][X] + dist[X][i]);
-        }
-        System.out.println(maxTime);
     }
 }
