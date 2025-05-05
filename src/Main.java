f14d6e25b624f5f68e0f6b69b2dd39b8667616df
akak4456 <akak4456@naver.com>
2025-05-05 16:22:55 +0900

    [CNG] 2206번: 벽 부수고 이동하기 풀었음

--- src/Main.java
+++ src/Main.java
@@ -4,63 +4,87 @@ import java.io.InputStreamReader;
 import java.util.*;
 import java.util.StringTokenizer;
 
+class GraphNode {
+    int row;
+    int col;
+    int curDist;
+    boolean isBreakingWallPossible;
+    public GraphNode(int row, int col, int curDist, boolean isBreakingWallPossible) {
+        this.row = row;
+        this.col = col;
+        this.curDist = curDist;
+        this.isBreakingWallPossible = isBreakingWallPossible;
+    }
+}
+
 public class Main {
-    private static int TC;
-    private static int N, M, W;
-    private static int dist[][];
-    private static final int INF = 987654321;
+    private static int N, M;
+    private static int[][] board;
+    private static Queue<GraphNode> queue;
+    private static boolean[][] visitedWhenIsBreakingWallPossible;
+    private static boolean[][] visitedWhenIsNotBreakingWallPossible;
+    private static int[] drow = {-1,1,0,0};
+    private static int[] dcol = {0,0,-1,1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        TC = Integer.parseInt(br.readLine());
-        for(int testCase = 0; testCase < TC; testCase++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            dist = new int[N + 1][N + 1];
-            for(int i=1;i<=N;i++) {
-                for(int j=1;j<=N;j++) {
-                    if(i == j) {
-                        dist[i][j] = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for(int j = 0; j < M; j++){
+                board[i][j] = line.charAt(j) - '0';
+            }
+        }
+        visitedWhenIsNotBreakingWallPossible = new boolean[N][M];
+        visitedWhenIsBreakingWallPossible = new boolean[N][M];
+        queue = new LinkedList<>();
+        queue.add(new GraphNode(0,0,1,true));
+        int result = Integer.MAX_VALUE;
+        while(!queue.isEmpty()) {
+            GraphNode node = queue.poll();
+
+            if(node.row == N - 1 && node.col == M - 1) {
+                result = Math.min(result, node.curDist);
+                continue;
+            }
+            if(node.isBreakingWallPossible) {
+                if(visitedWhenIsBreakingWallPossible[node.row][node.col]) {
+                    continue;
+                } else {
+                    visitedWhenIsBreakingWallPossible[node.row][node.col] = true;
+                }
+            }
+            if(!node.isBreakingWallPossible){
+                if(visitedWhenIsNotBreakingWallPossible[node.row][node.col]) {
+                    continue;
+                } else {
+                    visitedWhenIsNotBreakingWallPossible[node.row][node.col] = true;
+                }
+            }
+
+            for(int i=0;i<4;i++) {
+                int nrow = drow[i] + node.row;
+                int ncol = dcol[i] + node.col;
+                if(nrow < 0 || nrow >= N || ncol < 0 || ncol >= M) continue;
+                if(node.isBreakingWallPossible) {
+                    if(board[nrow][ncol] == 1) {
+                        queue.add(new GraphNode(nrow, ncol, node.curDist + 1,false));
                     } else {
-                        dist[i][j] = INF;
+                        queue.add(new GraphNode(nrow, ncol, node.curDist + 1,true));
+                    }
+                } else {
+                    if(board[nrow][ncol] == 0) {
+                        queue.add(new GraphNode(nrow, ncol, node.curDist + 1,false));
                     }
                 }
             }
-            for(int i=1;i<=M;i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                int t = Integer.parseInt(st.nextToken());
-                dist[a][b] = Math.min(dist[a][b], t);
-                dist[b][a] = Math.min(dist[b][a], t);
-            }
-            for(int i=1;i<=W;i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                int t = Integer.parseInt(st.nextToken());
-                dist[a][b] = -t;
-            }
-            for(int k=1;k<=N;k++) {
-                for(int i=1;i<=N;i++) {
-                    for(int j=1;j<=N;j++) {
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
-                }
-            }
-            boolean isPossible = false;
-            for(int i=1;i<=N;i++) {
-                if(dist[i][i] < 0) {
-                    isPossible = true;
-                    break;
-                }
-            }
-            if(isPossible) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
+        }
+        if(result == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(result);
         }
     }
 }
